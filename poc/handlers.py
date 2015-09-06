f4b2f3c1c6e55bb781464f280b805c364a17bcb1
S Anand <root.node@gmail.com>
2015-09-06 18:56:27 +0530

    Define and use Function handler

--- poc/handlers.py
+++ poc/handlers.py
@@ -1,10 +1,13 @@
 import tornado.web
+from confutil import python_name
 
 
-class TemplateHandler(tornado.web.RequestHandler):
-    def initialize(self, **kwargs):
+class Function(tornado.web.RequestHandler):
+    def initialize(self, function, kwargs={}, redirect=None):
+        self.function = python_name(function)
         self.kwargs = kwargs
+        self.redirect_url = redirect
 
     def get(self):
-        self.write('TemplateHandler:<p>{:s}</p>'.format(
-            str(self.kwargs)))
+        self.function(**self.kwargs)
+        self.redirect(self.redirect_url or self.request.headers.get('Referer', '/'))
