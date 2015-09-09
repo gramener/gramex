9257ea113c28f71ea7c6d0e6a25f1f350d6bcd14
S Anand <root.node@gmail.com>
2015-09-09 08:58:38 +0530

    DirectoryHandler subclasses StaticFileHandler

--- gramex/handlers.py
+++ gramex/handlers.py
@@ -1,9 +1,9 @@
 import os
-import tornado.web
+from tornado.web import HTTPError, RequestHandler, StaticFileHandler
 from .confutil import python_name
 
 
-class Function(tornado.web.RequestHandler):
+class Function(RequestHandler):
     def initialize(self, function, kwargs={}, redirect=None):
         self.function = python_name(function)
         self.kwargs = kwargs
@@ -14,21 +14,50 @@ class Function(tornado.web.RequestHandler):
         self.redirect(self.redirect_url or self.request.headers.get('Referer', '/'))
 
 
-class DirectoryHandler(tornado.web.RequestHandler):
-    def initialize(self, path):
-        self.root = path
+class DirectoryHandler(StaticFileHandler):
+    def validate_absolute_path(self, root, absolute_path):
+        '''
+        Return directory itself for directory
+        '''
+        root = os.path.abspath(root) + os.path.sep
+        # The trailing slash also needs to be temporarily added back
+        # the requested path so a request to root/ will match.
+        if not (absolute_path + os.path.sep).startswith(root):
+            raise HTTPError(403, "%s is not in root static directory",
+                            self.path)
+        if os.path.isdir(absolute_path):
+            if not self.request.path.endswith("/"):
+                self.redirect(self.request.path + "/", permanent=True)
+                return
+            if self.default_filename is not None:
+                default_file = os.path.join(absolute_path, self.default_filename)
+                if os.path.isfile(default_file):
+                    return default_file
+            # Now, we have a directory ending with "/" without a
+            # default_filename, so just allow it.
+            return absolute_path
+        if not os.path.exists(absolute_path):
+            raise HTTPError(404)
+        if not os.path.isfile(absolute_path):
+            raise HTTPError(403, "%s is not a file", self.path)
+        return absolute_path
 
-    def get(self, path):
-        paths = self.request.uri.split('/', 2)
-        fullpath = os.path.join(self.root, paths[-1])
+    @classmethod
+    def get_content(cls, abspath, start=None, end=None):
+        if os.path.isdir(abspath):
+            body = ['<h1>Index of %s </h1><ul>' % abspath]
+            for name in os.listdir(abspath):
+                body.append('<li><a href="%s">%s</a></li>' % (name, name))
+            body.append('</ul>')
+            return ''.join(body)
+        return super(DirectoryHandler, cls).get_content(abspath, start, end)
 
-        if os.path.isfile(fullpath):
-            self.render(fullpath)
-        elif os.path.exists(fullpath):
-            body = '<h1>Index of %s </h1>' % fullpath
-            for name in os.listdir(fullpath):
-                body += ('<p><a href="%s">%s</a></p>' %
-                         (name, name))
-            self.write(body)
-        else:
-            self.write('Random Request')
+    def get_content_size(self):
+        if os.path.isdir(self.absolute_path):
+            return len(self.get_content(self.absolute_path))
+        return super(DirectoryHandler, self).get_content_size()
+
+    def get_content_type(self):
+        if os.path.isdir(self.absolute_path):
+            return 'text/html'
+        return super(DirectoryHandler, self).get_content_type()
