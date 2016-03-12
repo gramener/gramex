7cb5fca2073ab4f6f87f0038f9d37a57ba3983fd
Pratap Vardhan <pratap.vardhan@gramener.com>
2016-03-12 21:06:00 +0530

    ENH: Add FacebookOAuth support via gramex.handlers.FacebookAuth

--- gramex/handlers/__init__.py
+++ gramex/handlers/__init__.py
@@ -5,8 +5,8 @@ Handlers
 from functionhandler import FunctionHandler
 from directoryhandler import DirectoryHandler
 from datahandler import DataHandler
-from authhandler import GoogleAuth
+from authhandler import GoogleAuth, FacebookAuth
 
 
 __all__ = ['FunctionHandler', 'DirectoryHandler', 'DataHandler',
-           'GoogleAuth']
+           'GoogleAuth', 'FacebookAuth']
--- gramex/handlers/authhandler.py
+++ gramex/handlers/authhandler.py
@@ -3,7 +3,7 @@ import json
 import tornado.web
 from tornado.web import RequestHandler
 from tornado.escape import json_encode
-from tornado.auth import GoogleOAuth2Mixin
+from tornado.auth import GoogleOAuth2Mixin, FacebookGraphMixin
 
 
 class GoogleAuth(RequestHandler, GoogleOAuth2Mixin):
@@ -34,3 +34,27 @@ class GoogleAuth(RequestHandler, GoogleOAuth2Mixin):
                 scope=['profile', 'email'],
                 response_type='code',
                 extra_params={'approval_prompt': 'auto'})
+
+
+class FacebookAuth(RequestHandler, FacebookGraphMixin):
+    @tornado.gen.coroutine
+    def get(self):
+        redirect_uri = (self.request.protocol + '://' + self.request.host +
+                        self.request.path)
+        if self.get_argument("code", False):
+            user = yield self.get_authenticated_user(
+                redirect_uri=redirect_uri,
+                client_id=self.settings["facebook_api_key"],
+                client_secret=self.settings["facebook_secret"],
+                code=self.get_argument("code"))
+            self.set_secure_cookie(self.settings['cookie_secret'],
+                                   str(time.time()))
+            self.set_secure_cookie('user', json_encode(user['name']))
+            self.redirect('/')
+        else:
+            yield self.authorize_redirect(
+                redirect_uri=redirect_uri,
+                client_id=self.settings["facebook_api_key"],
+                extra_params={
+                    'fields': 'name,email,first_name,last_name,'
+                              'gender,link,username,locale,timezone'})
