42ce26a11e7c5e6ff5d9e0cbfea004dcd568b52a
Pratap Vardhan <pratap.vardhan@gramener.com>
2016-03-12 21:02:46 +0530

    ENH: Add GoogleOAuth support via gramex.handlers.GoogleAuth

--- gramex/handlers/__init__.py
+++ gramex/handlers/__init__.py
@@ -5,5 +5,8 @@ Handlers
 from functionhandler import FunctionHandler
 from directoryhandler import DirectoryHandler
 from datahandler import DataHandler
+from authhandler import GoogleAuth
 
-__all__ = ['FunctionHandler', 'DirectoryHandler', 'DataHandler']
+
+__all__ = ['FunctionHandler', 'DirectoryHandler', 'DataHandler',
+           'GoogleAuth']
---
+++ gramex/handlers/authhandler.py
@@ -0,0 +1,36 @@
+import time
+import json
+import tornado.web
+from tornado.web import RequestHandler
+from tornado.escape import json_encode
+from tornado.auth import GoogleOAuth2Mixin
+
+
+class GoogleAuth(RequestHandler, GoogleOAuth2Mixin):
+    @tornado.gen.coroutine
+    def get(self):
+        redirect_uri = (self.request.protocol + '://' +
+                        self.request.host +
+                        self.request.path)
+        if self.get_argument('code', False):
+            access = yield self.get_authenticated_user(
+                redirect_uri=redirect_uri,
+                code=self.get_argument('code'))
+
+            http_client = self.get_auth_http_client()
+            response = yield http_client.fetch(
+                'https://www.googleapis.com/oauth2/v1/userinfo?access_token=' +
+                access["access_token"])
+            user = json.loads(response.body)["email"]
+
+            self.set_secure_cookie(self.settings['cookie_secret'],
+                                   str(time.time()))
+            self.set_secure_cookie('user', json_encode(user))
+            self.redirect('/')
+        else:
+            yield self.authorize_redirect(
+                redirect_uri=redirect_uri,
+                client_id=self.settings['google_oauth'].key,
+                scope=['profile', 'email'],
+                response_type='code',
+                extra_params={'approval_prompt': 'auto'})
