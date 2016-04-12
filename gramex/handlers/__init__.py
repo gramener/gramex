1430ad34090e0b420f3d30d2c2ffb778825b70b3
S Anand <root.node@gmail.com>
2016-04-12 14:43:25 +0530

    ENH: Basic ProcessHandler implementation. Fixes #14

--- gramex/handlers/__init__.py
+++ gramex/handlers/__init__.py
@@ -6,9 +6,10 @@ from .functionhandler import FunctionHandler
 from .filehandler import FileHandler
 from .datahandler import DataHandler
 from .authhandler import (GoogleAuth, FacebookAuth, TwitterAuth)
+from .processhandler import ProcessHandler
 
 
 DirectoryHandler = FileHandler
 
 __all__ = ['FunctionHandler', 'FileHandler', 'DirectoryHandler', 'DataHandler',
-           'GoogleAuth', 'FacebookAuth', 'TwitterAuth']
+           'GoogleAuth', 'FacebookAuth', 'TwitterAuth', 'ProcessHandler']
---
+++ gramex/handlers/processhandler.py
@@ -0,0 +1,209 @@
+import io
+import sys
+import subprocess
+import tornado.web
+import tornado.gen
+from threading import Thread, RLock
+from tornado.concurrent import Future
+
+try:
+    from Queue import Queue  # Python 2.7
+except ImportError:
+    from queue import Queue  # Python 3.x
+
+from .basehandler import BaseHandler
+from ..transforms import build_transform
+
+
+class ProcessHandler(BaseHandler):
+    '''
+    Runs sub-processes with transformations. It accepts these parameters:
+
+    :arg list/string args: The first value is the command. The rest are optional
+        arguments. This is the same as in `Popen`_.
+    :arg boolean shell: ``True`` passes the ``args`` through the shell, allowing
+        wildcards like ``*``. If ``shell=True`` then use a single string for
+        ``args`` that includes the arguments.
+    :arg string cwd: Current working directory from where the command will run.
+        Defaults to the same directory Gramex ran from.
+    :arg string stdout: The process output can be sent to:
+
+        - ``pipe``: Display the (transformed) output. This is the default
+        - ``null``: Ignore the output
+        - ``filename.txt``: Save output to a ``filename.txt``
+
+    :arg string stderr: The process error stream has the same options as stdout.
+        It can also be set to just ``stdout`` to re-use the stdout option.
+    :arg string stdin: Not yet implemented
+    :arg int/string buffer: 'line' will write lines as they are generated.
+        Numbers indicate the number of bytes to buffer. Defaults to
+        ``io.DEFAULT_BUFFER_SIZE``.
+    :arg string redirect: URL to redirect to when the result is done. Used to
+        trigger calculations without displaying any output.
+    :arg dict headers: HTTP headers to set on the response.
+    :arg dict transform: Transformations that should be applied to the files.
+        The key matches a `glob pattern`_ (e.g. ``'*.md'`` or ``'data/*'``.) The
+        value is a dict with the same structure as :class:`FunctionHandler`,
+        and accepts these keys:
+
+        ``encoding``
+            The encoding to load the file as. If you don't specify an encoding,
+            file contents are passed to ``function`` as a binary string.
+
+        ``function``
+            A string that resolves into any Python function or method (e.g.
+            ``markdown.markdown``). By default, it is called with the file
+            contents as ``function(content)`` and the result is rendered as-is
+            (hence must be a string.)
+
+        ``args``
+            optional positional arguments to be passed to the function. By
+            default, this is just ``['content']`` where ``content`` is the file
+            contents. You can also pass the handler via ``['handler']``, or both
+            of them in any order.
+
+        ``kwargs``:
+            an optional list of keyword arguments to be passed to the function.
+            A value with of ``handler`` and ``content`` is replaced with the
+            RequestHandler and file contents respectively.
+
+        ``headers``:
+            HTTP headers to set on the response.
+
+    .. _Popen: https://docs.python.org/3/library/subprocess.html#subprocess.Popen
+
+    '''
+
+    def initialize(self, args, shell=False, cwd=None, stdout='pipe', stderr='stdout', stdin=None,
+                   buffer_size=0, redirect=None, headers={}, transform={}, **kwargs):
+        self.params = kwargs
+        self.args = args
+        self.shell = shell
+        self.cwd = cwd
+        self.redirect = redirect
+        self._write_lock = RLock()
+
+        # Replace stdout, stderr, stream values
+        stream_map = {
+            'pipe': subprocess.PIPE,        # show on Gramex console
+        }
+
+        def sub(s, **kwargs):
+            return kwargs.get(s, stream_map.get(s, s))
+        self.stdout = sub(stdout)
+        self.stderr = sub(stderr, stdout=self.stdout)
+        self.stdin = sub(stdin)
+        self.buffer_size = buffer_size
+
+        self.headers = headers
+        self.transform = {}
+        for pattern, trans in transform.items():
+            self.transform[pattern] = {
+                'function': build_transform(trans, vars={'content': None, 'handler': None}),
+                'headers': trans.get('headers', {}),
+                'encoding': trans.get('encoding'),
+            }
+
+        self.post = self.get
+
+    @tornado.gen.coroutine
+    def get(self, *path_args):
+        for header_name, header_value in self.headers.items():
+            self.set_header(header_name, header_value)
+
+        proc = _Subprocess(
+            self.args,
+            shell=self.shell,
+            stream_stdout=self._write,
+            stream_stderr=self._write,
+            buffer_size=self.buffer_size,
+        )
+        yield proc.wait_for_exit()
+
+    def _write(self, data):
+        with self._write_lock:
+            self.write(data)
+            self.flush()
+
+
+class _Subprocess(object):
+    '''
+    tornado.process.Subprocess does not work on Windows.
+    https://github.com/tornadoweb/tornado/issues/1585
+
+    This is a threaded alternative based on
+    http://stackoverflow.com/a/4896288/100904
+
+    Create an internal implementation that works. You can later expose it.
+    Don't worry about Tornado conventions. Just get it to work first.
+
+        proc = _Subprocess(
+            args,
+            on_stdout=self._write,
+            on_stderr=self._write,
+            **kwargs
+        )
+        yield proc.wait_for_exit()
+
+    '''
+    def __init__(self, args, stream_stdout=None, stream_stderr=None, buffer_size=0, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+        # self.proc.stdout & self.proc.stderr are streams with process output
+        kwargs['stdout'] = kwargs['stderr'] = subprocess.PIPE
+
+        # On UNIX, close all file descriptors except 0, 1, 2 before child
+        # process is executed. I've no idea why. Copied from
+        # http://stackoverflow.com/a/4896288/100904
+        kwargs['close_fds'] = 'posix' in sys.builtin_module_names
+
+        if hasattr(buffer_size, 'lower') and 'line' in buffer_size.lower():
+            def _write(stream, callback, future):
+                'Call callback with content from stream. On EOF mark future as done'
+                while True:
+                    content = stream.readline()
+                    if len(content) > 0:
+                        callback(content)
+                    else:
+                        stream.close()
+                        future.set_result('')
+                        break
+        else:
+            # If the buffer size is 0 or negative, use the default buffer size to read
+            if buffer_size <= 0:
+                buffer_size = io.DEFAULT_BUFFER_SIZE
+            def _write(stream, callback, future):
+                'Call callback with content from stream. On EOF mark future as done'
+                while True:
+                    content = stream.read(buffer_size)
+                    size = len(content)
+                    if size > 0:
+                        callback(content)
+                    if size < buffer_size:
+                        stream.close()
+                        future.set_result('')
+                        break
+
+        self.proc = subprocess.Popen(args, **kwargs)
+        self.queue = {}         # Stores the output as a queue of bytes
+        self.thread = {}        # Has the running threads
+        self.future = {}        # Stores the futures indicating stream close
+        callback = {
+            'stdout': stream_stdout,
+            'stderr': stream_stderr,
+        }
+        for stream in ('stdout', 'stderr'):
+            self.queue[stream] = q = Queue()
+            self.future[stream] = f = Future()
+            # Thread writes from self.proc.stdout to self.queue['stdout']
+            # and similarly for stderr
+            self.thread[stream] = t = Thread(
+                target=_write,
+                args=(getattr(self.proc, stream), callback[stream] or q.put, f),
+            )
+            t.daemon = True     # Thread dies with the program
+            t.start()
+
+    def wait_for_exit(self):
+        return list(self.future.values())
