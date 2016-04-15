1654191452162164ef80564d4d09bdb4bc350be9
S Anand <root.node@gmail.com>
2016-04-15 09:51:07 +0530

    TST: test ProcessHandler cwd: parameter

--- tests/test_processhandler.py
+++ tests/test_processhandler.py
@@ -10,23 +10,25 @@ tearDownModule = server.stop_gramex
 class TestProcessHandler(TestGramex):
     folder = os.path.dirname(os.path.abspath(__file__))
 
-    def check_result(self, url, cmd=None, cwd=None, stdout=True, stderr=True):
+    def check_result(self, url, code=200, cmd=None, cwd=None, stdout=True, stderr=True):
         cmd = cmd or self.folder + '/utils.py'
         cwd = cwd or gramex.paths.base.absolute()
         r = self.get(url)
-        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.status_code, code)
+        lines = [line.strip() for line in r.text.split('\n')]
         if stdout:
-            self.assertIn('stdout starts', r.text)
-            self.assertIn('stdout ends', r.text)
-            self.assertIn('sys.argv[0]: %s' % cmd, r.text)
-            self.assertIn('sys.argv[1]: a', r.text)
-            self.assertIn('sys.argv[2]: 1', r.text)
-            self.assertIn('sys.argv[3]: x y', r.text)
+            self.assertIn('stdout starts', lines)
+            self.assertIn('stdout ends', lines)
+            self.assertIn('os.getcwd: %s' % cwd, lines)
+            self.assertIn('sys.argv[0]: %s' % cmd, lines)
+            self.assertIn('sys.argv[1]: a', lines)
+            self.assertIn('sys.argv[2]: 1', lines)
+            self.assertIn('sys.argv[3]: x y', lines)
         else:
-            self.assertNotIn('stdout starts', r.text)
+            self.assertNotIn('stdout starts', lines)
         if stderr:
-            self.assertIn('stderr starts', r.text)
-            self.assertIn('stderr ends', r.text)
+            self.assertIn('stderr starts', lines)
+            self.assertIn('stderr ends', lines)
         else:
             self.assertNotIn('stderr starts', r.text)
 
@@ -38,4 +40,4 @@ class TestProcessHandler(TestGramex):
 
     def test_cwd(self):
         cwd = str(gramex.paths.base.absolute().parent)
-        self.check_result(url='/process/shell', cwd=cwd)
+        self.check_result(url='/process/cwd', cwd=cwd)
