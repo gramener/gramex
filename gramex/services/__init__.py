5a4f6c970a09668f5393e0545c328e324469ec47
S Anand <root.node@gmail.com>
2016-05-14 11:16:24 +0530

    REF: move old SSL cert check code into AuthHandler for lazy checking

--- gramex/services/__init__.py
+++ gramex/services/__init__.py
@@ -13,7 +13,6 @@ exists, a warning is raised.
 '''
 from __future__ import unicode_literals
 
-import ssl
 import yaml
 import logging
 import posixpath
@@ -26,7 +25,6 @@ import concurrent.futures
 import six.moves.urllib.parse as urlparse
 from six.moves import http_client
 from orderedattrdict import AttrDict
-from tornado.httpclient import HTTPClient, AsyncHTTPClient
 from . import scheduler
 from . import watcher
 from . import urlcache
@@ -62,29 +60,6 @@ def app(conf):
     else:
         info.app = tornado.web.Application(**conf.settings)
         info.app.listen(**conf.listen)
-
-        # The latest SSL certificates from certifi don't work for Google Auth.
-        # Do a one-time check to access accounts.google.com. If it throws an SSL
-        # error, switch to old SSL certificates. See
-        # https://github.com/tornadoweb/tornado/issues/1534
-
-        # Use HTTPClient to check instead of AsyncHTTPClient because it's synchronous.
-        _client = HTTPClient()
-        try:
-            # Use accounts.google.com because we know it fails with new certifi certificates
-            # cdn.redhat.com is another site that fails.
-            _client.fetch("https://accounts.google.com/")
-        except ssl.SSLError:
-            try:
-                import certifi      # noqa: late import to minimise dependencies
-                AsyncHTTPClient.configure(None, defaults=dict(ca_certs=certifi.old_where()))
-                logging.warn('Using old SSL certificates for compatibility')
-            except ImportError:
-                pass
-        except Exception:
-            # Ignore any other kind of exception
-            logging.warn('Gramex has no direct Internet connection.')
-        _client.close()
 
         def callback():
             'Called after all services are started. Opens browser if required'
