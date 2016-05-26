2a9c1a572643f3e87bdfe464eeaae54782b4c747
S Anand <root.node@gmail.com>
2016-05-26 08:47:10 +0530

    ENH: gramex run saves parameters, install/uninstall multiple apps

--- gramex/install.py
+++ gramex/install.py
@@ -66,6 +66,15 @@ apps_config['base'] = PathConfig(gramex.paths['source'] / 'apps.yaml')
 user_config_file = app_dir / 'apps.yaml'
 apps_config['user'] = PathConfig(user_config_file)
 
+app_keys = {
+    'url': 'URL / filename of a ZIP file to install',
+    'rootdir': 'Sub-directory under "url" to install (optional)',
+    'contentdir': 'Strip root directory with a single child (optional, default=True)',
+    'target': 'Local directory where the app is installed',
+    'installed': 'Additional installation information about the app',
+    'run': 'Runtime keyword arguments for the app',
+}
+
 
 def save_user_config(appname, value):
     user_config = AttrDict()
@@ -77,8 +86,7 @@ def save_user_config(appname, value):
             del user_config[appname]
     else:
         app_config = user_config.setdefault(appname, AttrDict())
-        app_config.update(value)
-        app_config['installed_time'] = datetime.datetime.utcnow()
+        app_config.update({key: value[key] for key in app_keys if key in value})
 
     with user_config_file.open(mode='w', encoding='utf-8') as handle:
         yaml.dump(user_config, handle, indent=4, default_flow_style=False)
@@ -95,22 +103,25 @@ def install(cmd, args):
         apps = (+apps_config).keys()
         logging.error('gramex install [%s]', '|'.join(apps))
         return
+    if len(cmd) > 1 and args:
+        logging.error('Arguments allowed only with single app. Ignoring %s', ', '.join(cmd[1:]))
+        cmd = cmd[:1]
 
-    appname = cmd.pop(0)
-    logging.info('Installing: %s', appname)
-
-    app_config = get_app_config(appname, args)
-    # Download the app URL into target directory
-    if 'url' in app_config:
-        download_zip(
-            url=app_config.url,
-            target=app_config.target,
-            contentdir=app_config.get('contentdir', True),
-            rootdir=app_config.get('rootdir', None),
-        )
-        save_user_config(appname, app_config)
-    else:
-        logging.error('Cannot find URL to install %s. Use --url=...', appname)
+    for appname in cmd:
+        logging.info('Installing: %s', appname)
+        app_config = get_app_config(appname, args)
+        # Download the app URL into target directory
+        if 'url' in app_config:
+            download_zip(
+                url=app_config.url,
+                target=app_config.target,
+                contentdir=app_config.get('contentdir', True),
+                rootdir=app_config.get('rootdir', None),
+            )
+            app_config['installed'] = {'time': datetime.datetime.utcnow()}
+            save_user_config(appname, app_config)
+        else:
+            logging.error('Cannot find URL to install %s. Use --url=...', appname)
 
 
 def uninstall(cmd, args):
@@ -118,20 +129,23 @@ def uninstall(cmd, args):
         apps = (+apps_config['user']).keys()
         logging.error('gramex uninstall [%s]', '|'.join(apps))
         return
+    if len(cmd) > 1 and args:
+        logging.error('Arguments allowed only with single app. Ignoring %s', ', '.join(cmd[1:]))
+        cmd = cmd[:1]
 
-    appname = cmd.pop(0)
-    logging.info('Uninstalling: %s', appname)
+    for appname in cmd:
+        logging.info('Uninstalling: %s', appname)
 
-    # Delete the target directory if it exists
-    app_config = get_app_config(appname, args)
-    if os.path.exists(app_config.target):
-        if app_config.target.startswith(variables['GRAMEXDATA']):
-            shutil.rmtree(app_config.target)
+        # Delete the target directory if it exists
+        app_config = get_app_config(appname, args)
+        if os.path.exists(app_config.target):
+            if app_config.target.startswith(variables['GRAMEXDATA']):
+                shutil.rmtree(app_config.target)
+            else:
+                logging.warn('Not removing directory %s (outside $GRAMEXDATA)', app_config.target)
         else:
-            logging.warn('Not removing directory %s (outside $GRAMEXDATA)', app_config.target)
-    else:
-        logging.error('No directory %s to remove', app_config.target)
-    save_user_config(appname, None)
+            logging.error('No directory %s to remove', app_config.target)
+        save_user_config(appname, None)
 
 
 def run(cmd, args):
@@ -139,6 +153,8 @@ def run(cmd, args):
         apps = (+apps_config['user']).keys()
         logging.error('gramex run [%s]', '|'.join(apps))
         return
+    if len(cmd) > 1:
+        logging.error('Can only run one app. Ignoring %s', ', '.join(cmd[1:]))
 
     appname = cmd.pop(0)
     logging.info('Initializing %s on Gramex %s', appname, gramex.__version__)
@@ -147,6 +163,11 @@ def run(cmd, args):
     if os.path.exists(app_config.target):
         os.chdir(app_config.target)
         gramex.paths['base'] = Path('.')
-        gramex.init(cmd=AttrDict(app=args))
+        # If we run with updated parameters, save them for the next run
+        app_config.update({key: val for key, val in args.items() if key in app_keys})
+        app_config.setdefault('run', {}).update(
+            {key: val for key, val in args.items() if key not in app_keys})
+        save_user_config(appname, app_config)
+        gramex.init(cmd=AttrDict(app=app_config['run']))
     else:
         logging.error('No directory %s to run', app_config.target)
