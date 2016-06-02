11713f5b4523d940413406f24b4e3907e06fbb6c
S Anand <root.node@gmail.com>
2016-06-02 11:36:06 +0530

    REF: move check_old_certs() from AuthHandler into new gramex.utils

--- gramex/handlers/authhandler.py
+++ gramex/handlers/authhandler.py
@@ -1,11 +1,9 @@
-import ssl
 import time
 import tornado.web
 from tornado.web import RequestHandler
 from tornado.escape import json_encode, json_decode
-from tornado.httpclient import HTTPClient, AsyncHTTPClient
 from tornado.auth import GoogleOAuth2Mixin, FacebookGraphMixin, TwitterMixin
-from gramex.config import app_log
+from .. import utils
 
 
 def now():
@@ -17,38 +15,9 @@ class AuthHandler(RequestHandler):
     The parent handler for all Auth handlers. Does not derive from BaseHandler.
     It does not support caching or get_current_user().
     '''
-    ssl_checked = False
-
     @classmethod
-    def check_old_certs(cls):
-        '''
-        The latest SSL certificates from certifi don't work for Google Auth. Do
-        a one-time check to access accounts.google.com. If it throws an SSL
-        error, switch to old SSL certificates. See
-        https://github.com/tornadoweb/tornado/issues/1534
-        '''
-        if not cls.ssl_checked:
-            # Use HTTPClient to check instead of AsyncHTTPClient because it's synchronous.
-            _client = HTTPClient()
-            try:
-                # Use accounts.google.com because we know it fails with new certifi certificates
-                # cdn.redhat.com is another site that fails.
-                _client.fetch("https://accounts.google.com/")
-            except ssl.SSLError:
-                try:
-                    import certifi      # noqa: late import to minimise dependencies
-                    AsyncHTTPClient.configure(None, defaults=dict(ca_certs=certifi.old_where()))
-                    app_log.warn('Using old SSL certificates for compatibility')
-                except ImportError:
-                    pass
-            except Exception:
-                # Ignore any other kind of exception
-                app_log.warn('Gramex has no direct Internet connection.')
-            _client.close()
-            cls.ssl_checked = True
-
-    def initialize(self, **kwargs):
-        self.check_old_certs()
+    def setup(self, **kwargs):
+        utils.check_old_certs()
 
 
 class GoogleAuth(AuthHandler, GoogleOAuth2Mixin):
