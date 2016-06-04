09e0a2491f8b8091edf0d67aa5959f659f777252
S Anand <root.node@gmail.com>
2016-06-04 10:02:15 +0530

    REF: standardise user/session/redirection across handlers

--- gramex/handlers/authhandler.py
+++ gramex/handlers/authhandler.py
@@ -1,22 +1,16 @@
-import time
 import logging
+import functools
 import tornado.web
 import tornado.gen
-from tornado.escape import json_encode, json_decode
-from tornado.auth import GoogleOAuth2Mixin, FacebookGraphMixin, TwitterMixin
+from tornado.auth import (GoogleOAuth2Mixin, FacebookGraphMixin, TwitterMixin,
+                          urllib_parse, _auth_return_future)
 from gramex.config import check_old_certs, app_log
 from gramex.services import info
 from .basehandler import BaseHandler
 
 
-def now():
-    return '{:0.3f}'.format(time.time())
-
-
 class AuthHandler(BaseHandler):
-    '''
-    The parent handler for all Auth handlers.
-    '''
+    '''The parent handler for all Auth handlers.'''
     @classmethod
     def setup(cls, **kwargs):
         super(AuthHandler, cls).setup(**kwargs)
@@ -31,23 +25,33 @@ class GoogleAuth(AuthHandler, GoogleOAuth2Mixin):
             access = yield self.get_authenticated_user(
                 redirect_uri=redirect_uri,
                 code=self.get_argument('code'))
-
-            http_client = self.get_auth_http_client()
-            response = yield http_client.fetch(
-                'https://www.googleapis.com/oauth2/v1/userinfo?access_token=' +
-                access["access_token"])
-            user = json_decode(response.body)["email"]
-
-            self.set_secure_cookie(self.settings['cookie_secret'], now())
-            self.set_secure_cookie('user', json_encode(user))
-            self.redirect('/')
+            self.session['user'] = yield self.oauth2_request(
+                "https://www.googleapis.com/oauth2/v1/userinfo",
+                access_token=access["access_token"])
+            self.redirect(self.session.get('next', '/'))
         else:
             yield self.authorize_redirect(
                 redirect_uri=redirect_uri,
-                client_id=self.settings['google_oauth'].key,
+                client_id=self.conf.kwargs['key'],
                 scope=['profile', 'email'],
                 response_type='code',
                 extra_params={'approval_prompt': 'auto'})
+
+    @_auth_return_future
+    def get_authenticated_user(self, redirect_uri, code, callback):
+        '''Override this method to use self.conf.kwargs instead of self.settings'''
+        http = self.get_auth_http_client()
+        body = urllib_parse.urlencode({
+            "redirect_uri": redirect_uri,
+            "code": code,
+            "client_id": self.conf.kwargs['key'],
+            "client_secret": self.conf.kwargs['secret'],
+            "grant_type": "authorization_code",
+        })
+        http.fetch(self._OAUTH_ACCESS_TOKEN_URL,
+                   functools.partial(self._on_access_token, callback),
+                   method="POST", body=body,
+                   headers={'Content-Type': 'application/x-www-form-urlencoded'})
 
 
 class FacebookAuth(AuthHandler, FacebookGraphMixin):
@@ -55,35 +59,35 @@ class FacebookAuth(AuthHandler, FacebookGraphMixin):
     def get(self):
         redirect_uri = '{0.protocol:s}://{0.host:s}{0.path:s}'.format(self.request)
         if self.get_argument("code", False):
-            user = yield self.get_authenticated_user(
+            self.session['user'] = yield self.get_authenticated_user(
                 redirect_uri=redirect_uri,
-                client_id=self.settings["facebook_api_key"],
-                client_secret=self.settings["facebook_secret"],
-                code=self.get_argument("code"))
-            self.set_secure_cookie(self.settings['cookie_secret'], now())
-            self.set_secure_cookie('user', json_encode(user['name']))
-            self.redirect('/')
+                client_id=self.conf.kwargs['key'],
+                client_secret=self.conf.kwargs['secret'],
+                code=self.get_argument('code'))
+            self.redirect(self.session.get('next', '/'))
         else:
             yield self.authorize_redirect(
                 redirect_uri=redirect_uri,
-                client_id=self.settings["facebook_api_key"],
+                client_id=self.conf.kwargs['key'],
                 extra_params={
-                    'fields': 'name,email,first_name,last_name,'
-                              'gender,link,username,locale,timezone'})
+                    'fields': ','.join(self.conf.kwargs.get('fields', [
+                        'name', 'email', 'first_name', 'last_name', 'gender',
+                        'link', 'username', 'locale', 'timezone',
+                    ])),
+                })
 
 
 class TwitterAuth(AuthHandler, TwitterMixin):
     @tornado.gen.coroutine
     def get(self):
-        # TODO: Test
-        redirect_uri = '{0.protocol:s}://{0.host:s}{0.path:s}'.format(self.request)
         if self.get_argument("oauth_token", None):
-            user = yield self.get_authenticated_user()
-            self.set_secure_cookie(self.settings['cookie_secret'], now())
-            self.set_secure_cookie('user', json_encode(user['username']))
-            self.redirect('/')
+            self.session['user'] = yield self.get_authenticated_user()
+            self.redirect(self.session.get('next', '/'))
         else:
-            yield self.authenticate_redirect(callback_uri=redirect_uri)
+            yield self.authenticate_redirect()
+
+    def _oauth_consumer_token(self):
+        return dict(key=self.conf.kwargs['key'], secret=self.conf.kwargs['secret'])
 
 
 @tornado.gen.coroutine
@@ -142,13 +146,18 @@ class LDAPAuth(AuthHandler):
             raise tornado.gen.Return()
 
         import ldap
+
+        # As setup() is not a coroutine, do a one-time TLS setup
         if not self.tls_checked and self.conf.kwargs.get('tls'):
+            # Do not repeat the TLS check again
             self.tls_checked.add(True)
             try:
                 yield info.threadpool.submit(self.ldap.start_tls_s)
             except Exception:
+                # If we can't connect via TLS, that's OK, just continue
                 app_log.error('LDAP: Cannot connect securely to %s', self.url, exc_info=True)
 
+        # Search for the given user
         try:
             user_dn = yield async(self.ldap.search, self.base, ldap.SCOPE_SUBTREE,
                                   '(%s=%s)' % (self.user_attr, user))
@@ -156,12 +165,18 @@ class LDAPAuth(AuthHandler):
             report_error(logging.ERROR, code='search', exc_info=True)
         if not user_dn:
             report_error(logging.INFO, code='user')
+
+        # Check for the password
         try:
             yield async(self.ldap.simple_bind, user_dn[0][0], password)
-        except ldap.INVALID_CREDENTIALS, ldap.UNWILLING_TO_PERFORM:
+        except (ldap.INVALID_CREDENTIALS, ldap.UNWILLING_TO_PERFORM):
             report_error(logging.INFO, code='password', exc_info=False)
         except ldap.LDAPError:
             report_error(logging.ERROR, code='other', exc_info=True)
 
-        self.session['user'] = user_dn[0]
-        self.redirect('.')
+        # The return value is a list of search results. Take the first element.
+        # This is a tuple (dn, {user_info}). Just take the user_info dict, and
+        # add the dn as an item.
+        self.session['user'] = user_dn[0][1]
+        self.session['user']['dn'] = user_dn[0][0]
+        self.redirect(self.session.get('next', '/'))
