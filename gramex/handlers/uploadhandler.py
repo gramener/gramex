9aeecc66d5ea76fee71508abe261a521fd5499fc
Pratap Vardhan <pratap.vardhan@gramener.com>
2016-07-04 11:18:40 +0530

    Trasform in addfile method, Run addfiles in thread, write response back

--- gramex/handlers/uploadhandler.py
+++ gramex/handlers/uploadhandler.py
@@ -1,9 +1,12 @@
 import os
+import six
 import time
 import json
+import gramex
 import mimetypes
 import tornado.gen
 from orderedattrdict import AttrDict
+from gramex.transforms import build_transform
 from .basehandler import BaseHandler, HDF5Store
 
 
@@ -34,8 +37,9 @@ class FileUpload(object):
         self.store = HDF5Store(os.path.join(self.path, 'meta'))
         self.files = [self.store.load(k) for k in self.store.keys()]
 
-    def addfile(self, handler, *args, **kwargs):
+    def addfiles(self, handler, *args, **kwargs):
         files = handler.request.files
+        filemetas = []
         for upload in files.get('file', []):
             filename, ext = os.path.splitext(upload['filename'])
             filepath = self.uniq_filename(filename, ext)
@@ -51,9 +55,11 @@ class FileUpload(object):
                 'size': os.stat(filepath).st_size,
                 'mime': mime,
                 'data': handler.request.arguments})
+            filemeta = handler.transforms(filemeta)
             self.files.append(filemeta)
             self.store.dump(filename_, filemeta)
-        return
+            filemetas.append(filemeta)
+        return filemetas
 
     def uniq_filename(self, name, ext):
         filepath = os.path.join(self.path, name + ext)
@@ -68,16 +74,29 @@ class FileUpload(object):
 
 class UploadHandler(BaseHandler):
     @classmethod
-    def setup(cls, **kwargs):
+    def setup(cls, transform={}, **kwargs):
         super(UploadHandler, cls).setup(**kwargs)
         cls.params = AttrDict(kwargs)
         cls.uploader = FileUpload(cls.params.path)
 
+        cls.transform = []
+        if 'function' in transform:
+            cls.transform.append(build_transform(transform, vars=AttrDict(content=None),
+                                                 filename='url>%s' % cls.name))
+
     @tornado.gen.coroutine
     def post(self, *args, **kwargs):
-        self.uploader.addfile(self)
-        self.write('File uploaded!')
+        content = yield gramex.service.threadpool.submit(self.uploader.addfiles, self)
+        if not isinstance(content, (six.binary_type, six.text_type)):
+            content = json.dumps(content, ensure_ascii=True, separators=(',', ':'))
+        self.write(content)
 
     @tornado.gen.coroutine
     def get(self, *args, **kwargs):
         self.write(json.dumps(self.uploader.files))
+
+    def transforms(self, content):
+        for transform in self.transform:
+            for value in transform(content):
+                content = value
+        return content
