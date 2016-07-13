8c64d7e44ace57b2486d00d108119fbe9043fdae
Pratap Vardhan <pratap.vardhan@gramener.com>
2016-07-13 19:03:21 +0530

    ENH: Enable end users can authenticate their own account. TwitterRESTHandler will use their OAuth tokens

--- gramex/handlers/twitterresthandler.py
+++ gramex/handlers/twitterresthandler.py
@@ -4,10 +4,12 @@ import gramex
 import tornado.gen
 from oauthlib import oauth1
 from orderedattrdict import AttrDict
-from six.moves.http_client import OK
-from gramex.transforms import build_transform
+from six.moves.http_client import OK, NOT_FOUND
+from tornado.web import HTTPError
+from tornado.auth import TwitterMixin
 from tornado.httpclient import AsyncHTTPClient
 from tornado.httputil import url_concat, responses
+from gramex.transforms import build_transform
 from .basehandler import BaseHandler
 
 NETWORK_TIMEOUT = 599
@@ -16,7 +18,7 @@ custom_responses = {
 }
 
 
-class TwitterRESTHandler(BaseHandler):
+class TwitterRESTHandler(BaseHandler, TwitterMixin):
     @classmethod
     def setup(cls, transform={}, methods=['post'], **kwargs):
         super(TwitterRESTHandler, cls).setup(**kwargs)
@@ -36,6 +38,14 @@ class TwitterRESTHandler(BaseHandler):
     def run(self, path):
         args = {key: self.get_argument(key) for key in self.request.arguments}
         params = self.conf.kwargs
+        info = self.session.get('twitter_info', {})
+        # update params with session parameters if any
+        if not all(k in params for k in ('access_token', 'access_token_secret')):
+            if info:
+                params.update(info)
+            else:
+                raise HTTPError(NOT_FOUND, log_message='access_token missing')
+
         client = oauth1.Client(
             params.consumer_key,
             client_secret=params.consumer_secret,
@@ -74,3 +84,24 @@ class TwitterRESTHandler(BaseHandler):
             for value in transform(result):
                 result = value
         return result
+
+    @tornado.gen.coroutine
+    def get(self, path):
+        if self.get_argument('oauth_token', None):
+            info = yield self.get_authenticated_user()
+            info['access_token'].update({
+                'consumer_key': self.conf.kwargs['consumer_key'],
+                'consumer_secret': self.conf.kwargs['consumer_secret'],
+                'access_token': info['access_token']['key'],
+                'access_token_secret': info['access_token']['secret']})
+            # Store the information in the session, for now.
+            # Later, gramex.yaml can define where to store it Perhaps as a function
+            self.session['twitter_info'] = info['access_token']
+            self.redirect_next()
+        else:
+            self.save_redirect_page()
+            yield self.authorize_redirect()
+
+    def _oauth_consumer_token(self):
+        return dict(key=self.conf.kwargs['consumer_key'],
+                    secret=self.conf.kwargs['consumer_secret'])
