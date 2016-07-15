23a8b310616d5bce8fc2f0b6c80c587b069b0060
S Anand <root.node@gmail.com>
2016-07-15 16:52:46 +0530

    BLD: comment out nonexistent _static docs directory

--- docs/conf.py
+++ docs/conf.py
@@ -148,7 +148,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # here, relative to this directory. They are copied after the builtin
 # static files, so a file named "default.css" will overwrite the builtin
 # "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page
 # bottom, using the given strftime format.
