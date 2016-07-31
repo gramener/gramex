712b0b3a48d9202152b7329d2b36b31a2ff2337b
S Anand <root.node@gmail.com>
2016-07-31 08:20:38 +0530

    BUG: make datahandler guide fetch flags robustly

--- gramex/apps/guide/datahandler/fetch.py
+++ gramex/apps/guide/datahandler/fetch.py
@@ -2,6 +2,7 @@ import os.path
 import logging
 import pandas as pd
 from sqlalchemy import create_engine
+from sqlalchemy.exc import DatabaseError
 
 
 def data():
@@ -11,15 +12,21 @@ def data():
     url = 'https://gramener.com/flags/test.csv'
     folder = os.path.dirname(os.path.abspath(__file__))
     filepath = os.path.join(folder, 'database.sqlite3')
-
-    if os.path.exists(filepath):
-        logging.info('database.sqlite3 already present')
-        return
-
-    logging.info('fetch flags data and create database.sqlite3')
-    flags = pd.read_csv(url, encoding='cp1252')
     engine = create_engine('sqlite:///%s' % filepath, encoding='utf-8')
+    try:
+        flags = pd.read_sql_table('flags', engine)
+        assert len(flags) > 1
+        logging.info('database.sqlite3 has flags table with data')
+        return
+    except DatabaseError:
+        # If the database is corrupted, delete it
+        os.unlink(filepath)
+    except Exception:
+        pass
+
+    flags = pd.read_csv(url, encoding='cp1252')
     flags.to_sql('flags', engine, index=False)
+    logging.info('database.sqlite3 created with flags table')
 
 
 def bigint(content):
