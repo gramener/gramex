5143e1e36e1663a03780b7e7a70125543f5f6ca9
S Anand <root.node@gmail.com>
2017-06-21 11:08:35 +0530

    TST: add test case for application imported from subdirectory

---
+++ tests/subapp/subapp.py
@@ -0,0 +1,2 @@
+def run(handler):
+    return 'OK'
---
+++ tests/test_subapp.py
@@ -0,0 +1,8 @@
+from . import TestGramex
+
+
+class TestSubApp(TestGramex):
+    '''Test applications imported from subdirectories'''
+
+    def test_subapp(self):
+        self.check('/subapp/', text='OK')
