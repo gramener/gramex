5934e0ecbc59f1164ca895551403a367bd4dfdbb
S Anand <root.node@gmail.com>
2018-04-14 11:44:38 +0530

    TST: add CaptureHandler HTTP header test case

--- tests/test_capturehandler.py
+++ tests/test_capturehandler.py
@@ -15,6 +15,7 @@ from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfpage import PDFPage
 from pdfminer.high_level import extract_text_to_fp
 from gramex.handlers import Capture
+from gramex.services import info
 from . import TestGramex, server
 
 _captures = {}
@@ -61,7 +62,7 @@ class TestCaptureHandler(TestGramex):
         cls.capture = get_capture('default', port=9402)
         cls.folder = os.path.dirname(os.path.abspath(__file__))
 
-    def test_capture_init(self):
+    def test_init(self):
         ok_(self.capture.started)
 
     def fetch(self, url, code=200, **kwargs):
@@ -84,7 +85,7 @@ class TestCaptureHandler(TestGramex):
             if len(frag) > 0 and '<' not in line:
                 self.assertIn(frag, text)
 
-    def test_capture_pdf(self):
+    def test_pdf(self):
         # Test capture library API
         content = self.capture.pdf(url=server.base_url + self.url)
         self.check_pdf(content)
@@ -133,7 +134,7 @@ class TestCaptureHandler(TestGramex):
             self.assertGreater(colors.get(color, 0), min)
         return img
 
-    def test_capture_png(self):
+    def test_png(self):
         content = self.capture.png(url=server.base_url + self.url)
         self.check_img(content, color=(255, 0, 0, 255), min=100, size=self.size)
 
@@ -148,7 +149,7 @@ class TestCaptureHandler(TestGramex):
             'url': self.url, 'selector': '.subset', 'ext': 'png'})
         self.check_img(result.content, color=(0, 128, 0, 255), min=9000, size=(100, 100))
 
-    def test_capture_jpg(self):
+    def test_jpg(self):
         # TODO: check colors
         content = self.capture.jpg(url=server.base_url + self.url)
         self.check_img(content)
@@ -175,7 +176,7 @@ class TestCaptureHandlerChrome(TestCaptureHandler):
         if font_size is not None:
             eq_(shape.text_frame.paragraphs[0].runs[0].font.size, Pt(font_size))
 
-    def test_capture_pptx(self):
+    def test_pptx(self):
         content = self.capture.pptx(url=server.base_url + self.url)
         prs = Presentation(io.BytesIO(content))
         eq_(len(prs.slides), 1)
@@ -224,9 +225,32 @@ class TestCaptureHandlerChrome(TestCaptureHandler):
         self.assertAlmostEqual(prs.slides[1].shapes[0].width * 2, para_size[0], delta=1)
         self.assertAlmostEqual(prs.slides[1].shapes[0].height * 2, para_size[1], delta=1)
 
-    def test_capture_delay_render(self):
+    def test_delay_render(self):
         # delay=. After 1 second, renderComplete is set. Page changes text and color to green
         result = self.fetch(self.src, params={'url': self.url, 'delay': 'renderComplete',
                                               'file': 'delay-str'})
         self.check_filename(result, 'delay-str.pdf')
         self.assertIn('Greenblock', normalize(get_text(result.content)))
+
+    def test_headers(self):
+        # Headers are passed through, but cookie overrides it
+        result = self.fetch(self.src, params={'url': '/httpbin', 'cookie': 'right=1'}, headers={
+            'dnt': '1',
+            'user-agent': 'gramex-test',
+            'cookie': 'wrong=1'
+        })
+        text = get_text(result.content)
+        ok_(re.search(r'"dnt":\s+"1"', text, re.IGNORECASE))
+        ok_(re.search(r'"user\-agent":\s+"gramex-test"', text, re.IGNORECASE))
+        # ?cookie= should override header. Result must be right=1, not wrong=1
+        ok_('wrong' not in text)
+        ok_('right' in text)
+
+        # Check that the request can mimic a user
+        user = {'id': 'login@example.org', 'role': 'manager'}
+        result = self.fetch(self.src, params={'url': '/auth/session'}, headers={
+            'x-gramex-user': info['encrypt'].encrypt(user),
+        })
+        text = get_text(result.content)
+        ok_(user['id'] in text)
+        ok_(user['role'] in text)
