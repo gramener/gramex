1efa4ff324f7161ed907ce335ecc4109d70a2429
Pratap Vardhan <pratap.vardhan@gramener.com>
2019-03-27 15:17:28 +0530

    BUG: admin/auth config

--- gramex/apps/admin2/rollup.config.js
+++ gramex/apps/admin2/rollup.config.js
@@ -3,7 +3,7 @@ import htmlparts from 'rollup-plugin-htmlparts'
 export default [
   {
     input: 'schedule.src.js',
-    output: { file: "schedule.js", name: "schedule", format: "umd" },
+    output: { file: 'schedule.js', name: 'schedule', format: 'umd' },
     plugins: [
       htmlparts('schedule.template.html')
     ]
