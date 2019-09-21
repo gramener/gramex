6c8648980e2231d1f44b4a637a015f19272b0196
Bhanu K <bhanu.kamapantula@gramener.com>
2019-09-21 12:36:00 +0530

    ENH: pre-commit configuration in gramex init

---
+++ gramex/apps/init/setup.sh
@@ -0,0 +1,7 @@
+
+# installs pre-commit package for build errors checks
+pip install pre-commit
+# configures pre-commit hook in target_directory/.git/hooks/
+pre-commit install
+# after above step, a pre-commit file will be created in target_directory/.git/hooks/
+
