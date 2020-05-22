6382ca9b36a97491cfc75a2143549ca76ee3f140
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2020-05-22 18:44:59 +0530

    Explicitly install ui components

--- gramex/apps/filemanager/test/test.sh
+++ gramex/apps/filemanager/test/test.sh
@@ -1,13 +1,12 @@
 set -x
 
-cd ../ui/ && gramex setup .
-cd -
-yarn install
+gramex setup ui
+npm install
 browserify -s tape -r tape -o test/tape.js
 cd test
 nohup gramex &
 echo $! > gramex-pid.txt
-sleep 10
+sleep 5
 node puppet.js | tap-merge | faucet
 kill -9 `cat gramex-pid.txt`
 cat nohup.out
