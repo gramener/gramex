35ef8b3559ff943daf410cc395eff5aa961d14eb
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2020-05-22 08:24:19 +0530

    TEST: Add failhandler tests

--- gramex/apps/filemanager/filemanager.js
+++ gramex/apps/filemanager/filemanager.js
@@ -24,64 +24,46 @@ const DEFAULT_OPTS = {
     },
     {name: "tags", editable: {input: "text"}},
   ],
-  exportTemplate: '<button id="uploadbtn" class="btn btn-primary">Upload</button>',
-  notification: ".drivenotif"
+  exportTemplate: '<button id="uploadbtn" class="btn btn-primary">Upload</button>'
 }
-const NOTIFICATION_TEMPLATE = `
-<div class="alert alert-warning alert-dismissible" role="alert">
-  <template class="notif"><%= message %></template>
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-  </button>
-</div>`
+
 const DEFAULT_COLS = _.map(DEFAULT_OPTS.columns, "name")
 
-function attach_dropzone(el, opts, parent = null, clickable = true) {
-  if (!(parent)) { parent = el }
-  $(el).dropzone({
-    url: $(parent).attr('data-src'),
-    clickable: clickable,
-    createImageThumbnails: false,
-    previewTemplate: "<div></div>",
-    init: function() {
-      this.on('success', function(e) { renderTable(parent, opts) })
-      .on('error', function(file, error, xhr) {
-        $(opts.notification).html(NOTIFICATION_TEMPLATE)
-        if (xhr.status == 413) {
-          $("template.notif").template({message: 'File too large.'})
-        } else if (xhr.status == 415) {
-          $("template.notif").template({message: 'File type not allowed.'})
-        } else if (xhr.status == 0) {
-          $("template.notif").template({message: 'Server not reachable.'})
+function attach_dropzone(el, parent, url, clickable = true, success = null) {
+  try {
+    $(el).dropzone({
+      url: url,
+      clickable: clickable,
+      createImageThumbnails: false,
+      previewTemplate: "<div></div>",
+      init: function() {
+        this.on('success', success) // function(e) { renderTable(parent, opts) })
+        .on('error', $(parent).data("formhandler")._failHandler)
         }
       })
-    }
-  })
+  } catch (err) { if (err.message != "Dropzone already attached.") throw err }
 }
 
+// move it inside filemanager
 function renderTable(el, opts) {
-  let filecol = _.find(opts.columns, (c) => {return c.name == "file"})
+  let filecol = _.find(opts.columns, c => c.name == "file")
   filecol.link = filecol.link || el.dataset.src + DOWNLOAD_LINK
   $(el).on('load', function(e) {
     let btn = $(this).find('#uploadbtn').get(0)
-    if (!($(btn).hasClass('dz-clickable'))) {
-      attach_dropzone(btn, opts, el)
-    }
+    attach_dropzone(btn, el, el.dataset.src, true, renderTable.bind(this, el, opts))
     // Attach a dropzone to the whole element
-    if (!(el.dropzone)) {
-      attach_dropzone(el, opts, clickable = false)
-    }
+    attach_dropzone(el, el, el.dataset.src, false, renderTable.bind(this, el, opts))
   })
   .formhandler(opts)
 }
 
 $.fn.filemanager = function(opts) {
   opts = opts || {columns: []}
-  newopts = Object.assign({}, opts, DEFAULT_OPTS)
+  let newopts = Object.assign({}, opts, DEFAULT_OPTS)
   newopts.columns.forEach((s)=>{
-    Object.assign(s, _.find(opts.columns, (col) => {return col.name == s.name}))
+    Object.assign(s, _.find(opts.columns, col => col.name == s.name))
   })
-  newopts.columns.push(..._.filter(opts.columns, (c) => {return !(DEFAULT_COLS.includes(c.name))}))
+  newopts.columns.push(..._.filter(opts.columns, c => !(DEFAULT_COLS.includes(c.name))))
   this.each(function() {
     renderTable(this, newopts)
   })
