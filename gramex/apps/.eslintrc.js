f8caa8dd40a8d250f724684bb09dc257474ab5c3
S Anand <root.node@gmail.com>
2020-05-25 13:43:59 +0530

    WIP

--- gramex/apps/.eslintrc.js
+++ gramex/apps/.eslintrc.js
@@ -1,11 +1,16 @@
 /* eslint-env node */
+
+// This file has common options for all Gramex apps
+// See https://eslint.org/docs/user-guide/configuring
+// To override for specific apps, add it in package.json > eslintConfig
+// ... or add a .eslintrc.js
 module.exports = {
   'parserOptions': {
-    'ecmaVersion': 2017
+    'ecmaVersion': 6    // Use ES6 parser. Browsers other than IE support it
   },
+  // Styles are based on recommended eslint fields, but with specific overrides
   'extends': 'eslint:recommended',
   'rules': {
-    /* Override default rules */
     'indent': [2, 2, { 'VariableDeclarator': 2 }],  // Force 2 space indentation
     'linebreak-style': 2,           // Force UNIX style line
     'quotes': [1, 'single'],        // Prefer double-quotes style
