2d9a81b3c3559b221cab15a4d7e72e3f5426b0a9
S Anand <root.node@gmail.com>
2020-05-25 11:37:09 +0530

    Refactor tests

--- gramex/apps/filemanager/test/puppet.js
+++ gramex/apps/filemanager/test/puppet.js
@@ -1,7 +1,6 @@
 /* eslint no-console: 0 */
 
 const glob = require('glob')
-const port = 9988
 async function run_puppeteer() {
   const puppeteer = require('puppeteer')
   const browser = await puppeteer.launch({
@@ -9,18 +8,15 @@ async function run_puppeteer() {
     args: ['--no-sandbox']
   })
   const page = await browser.newPage()
-  // Note: if there's a console error, msg.type == 'error'
-  // msg.args has the error arguments.
-  page.on('console', msg => console.log(msg.text))    // eslint-disable-line no-console
+  // Note: if there's a console error, msg.type == 'error'. msg.args has error arguments.
+  page.on('console', msg => console.log(msg.text()))    // eslint-disable-line no-console
   const paths = glob.sync('test-*.html')
   for (let i = 0; i < paths.length; i++) {
-    let url = 'http://localhost:' + port + '/' + paths[i]
+    let url = 'http://localhost:9999/' + paths[i]
     await page.goto(url)
-    console.log(url)
     try {
       await page.waitForFunction('window.renderComplete')
     } catch (e) {
-      console.log(e)
       console.log('not ok ' + paths[i])
     }
   }
--- gramex/apps/filemanager/test/test.sh
+++ gramex/apps/filemanager/test/test.sh
@@ -1,11 +1,14 @@
-set -ex
-
-npm install
+# Create test/tape.js dynamically. It's not committed
 browserify -s tape -r tape -o test/tape.js
-cd test
-nohup gramex &
-echo $! > gramex-pid.txt
-sleep 5
+
+rm -f gramex.log
+gramex --listen.port=9999 > gramex.log 2>&1 &
+GRAMEX_PID=$!
+# Kill gramex when process exits. Note: Doesn't kill Gramex on Cygwin
+trap "kill $GRAMEX_PID" SIGINT SIGTERM EXIT
+
+# Wait until Gramex starts at port 9999
+until grep 9999 gramex.log; do sleep 0.2; done
+
+# Run test cases
 node puppet.js | tap-merge | faucet
-kill -9 `cat gramex-pid.txt`
-cd ..
