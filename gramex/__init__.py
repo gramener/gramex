f5bbb059b4a7915d50073d097efdeb29b1857b75
S Anand <root.node@gmail.com>
2020-08-12 09:21:42 +0530

    ENH: Ignore comment keys anywhere in config

--- gramex/__init__.py
+++ gramex/__init__.py
@@ -33,10 +33,9 @@ import logging
 import logging.config
 import tornado.ioloop
 from pathlib import Path
-from copy import deepcopy
 from orderedattrdict import AttrDict
 from gramex.config import ChainConfig, PathConfig, app_log, variables, setup_variables
-from gramex.config import ioloop_running
+from gramex.config import ioloop_running, prune_keys
 
 paths = AttrDict()              # Paths where configurations are stored
 conf = AttrDict()               # Final merged configurations
@@ -272,7 +271,7 @@ def init(force_reload=False, **kwargs):
         if key not in conf or conf[key] != val or force_reload:
             if hasattr(services, key):
                 app_log.debug('Loading service: %s', key)
-                conf[key] = deepcopy(val)
+                conf[key] = prune_keys(val, {'comment'})
                 callback = getattr(services, key)(conf[key])
                 if callable(callback):
                     callbacks[key] = callback
