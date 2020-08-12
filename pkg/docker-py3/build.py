ae3f2edcb15ab619a1b585eec42e716ab04ff2a6
S Anand <root.node@gmail.com>
2020-08-12 10:16:49 +0530

    Stream output, get all labels from release.json

--- pkg/docker-py3/build.py
+++ pkg/docker-py3/build.py
@@ -1,22 +1,14 @@
-import os
-from docker import from_env
-from tornado.template import Template
-import json
-from orderedattrdict import AttrDict
+import docker
+import gramex.cache
+import io
 
-
-client = from_env()
-folder = os.path.dirname(os.path.abspath(__file__))
-with open(os.path.join(folder, '../../gramex/release.json'), encoding='utf-8') as handle:
-    release = json.load(handle, object_pairs_hook=AttrDict)
-
-with open(os.path.join(folder, 'Dockerfile.tmpl'), encoding='utf8') as fin:
-    content = Template(fin.read()).generate(version=release.version)
-
-with open(os.path.join(folder, 'Dockerfile'), 'wb') as fout:
-    fout.write(content)
-
-_, log = client.images.build(path=folder, tag=f'gramener/gramex:{release.version}', rm=True)
-for msg in log:
-    for k, v in msg.items():
-        print(k, "\t", v)
+template = gramex.cache.open('Dockerfile.tmpl', 'template', rel=True)
+release = gramex.cache.open('../../gramex/release.json', rel=True)
+client = docker.from_env()
+streamer = client.api.build(
+    fileobj=io.BytesIO(template.generate(**release)),
+    tag=f'gramener/gramex:{release["version"]}',
+    decode=True,
+)
+for chunk in streamer:
+    print(chunk['stream'])  # noqa
