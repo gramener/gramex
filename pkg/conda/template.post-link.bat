a327a4eb290d478520b86a686ebe1797c4424136
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2020-08-24 12:00:31 +0530

    Move pip dependencies exclusively to post link

--- pkg/conda/template.post-link.bat
+++ pkg/conda/template.post-link.bat
@@ -1,3 +1,7 @@
+{% for entry in release.lib %}
+"%PREFIX%\Scripts\pip.exe" install --use-feature=2020-resolver "{% raw entry %}"
+{% end %}
+
 {% for entry in release.pip %}
 "%PREFIX%\Scripts\pip.exe" install --use-feature=2020-resolver "{% raw entry %}"
 {% end %}
