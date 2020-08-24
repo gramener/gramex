492bf6fa06c3743f69372d6699e4a123d82bd272
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2020-08-24 09:39:40 +0530

    Log messages

--- pkg/conda/template.post-link.bat
+++ pkg/conda/template.post-link.bat
@@ -1,10 +1,7 @@
-{% for entry in release.lib %}
-"%PREFIX%\Scripts\pip.exe" install --use-feature=2020-resolver "{% raw entry %}"
-{% end %}
 {% for entry in release.pip %}
 "%PREFIX%\Scripts\pip.exe" install --use-feature=2020-resolver "{% raw entry %}"
 {% end %}
 
 call "%PREFIX%\Library\bin\yarn.cmd" config set ignore-engines true
-"%PREFIX%\Scripts\gramex.exe" setup --all
+"%PREFIX%\Scripts\gramex.exe" setup --all > %PREFIX%\.messages.txt 2>&1
 if errorlevel 1 exit 1
