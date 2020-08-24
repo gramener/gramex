012d3cf3e04ca41100caa93f477c7ca833a2a17e
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2020-08-24 11:31:52 +0530

    Try installing only pip

--- pkg/conda/template.post-link.bat
+++ pkg/conda/template.post-link.bat
@@ -2,6 +2,4 @@
 "%PREFIX%\Scripts\pip.exe" install --use-feature=2020-resolver "{% raw entry %}"
 {% end %}
 
-call "%PREFIX%\Library\bin\yarn.cmd" config set ignore-engines true
-rem "%PREFIX%\Scripts\gramex.exe" setup ui > %PREFIX%\.messages.txt 2>&1
 if errorlevel 1 exit 1
