a1330466041e5633531f4484351f044fa2e35a60
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2020-08-25 07:45:44 +0530

    WIP

--- pkg/conda/template.post-link.bat
+++ pkg/conda/template.post-link.bat
@@ -1,5 +1,7 @@
 {% for entry in release.pip %}
 %PREFIX%\Scripts\pip.exe install --use-feature=2020-resolver {% raw entry %}
 {% end %}
+call %PREFIX%\Library\bin\yarn.cmd config set ignore-engines true
+%PREFIX%\Scripts\gramex.exe setup --all
 
 if errorlevel 1 exit 1
