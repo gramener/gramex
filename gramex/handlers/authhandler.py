gramener/gramex

commit f8edaeebf08c3c7bca31b10e6a7c3d755d2b9312

ENH: Use X-Request-URI for redirection. Fixes #289

gramex/handlers/authhandler.py
@@ -1,6 +1,5 @@
 import os
 import csv
-import six
 import json
 import time
 import uuid
@@ -10,6 +9,7 @@ import tornado.httpclient
 from tornado.auth import GoogleOAuth2Mixin
 from tornado.gen import coroutine, sleep
 from tornado.web import HTTPError
+from urllib.parse import urlencode
 from collections import Counter
 from orderedattrdict import AttrDict
 import gramex
@@ -205,7 +205,7 @@ class AuthHandler(BaseHandler):
         token = self.get_argument('recaptcha', None)
         if token is None:
             raise HTTPError(FORBIDDEN, "'recaptcha' argument missing from POST")
-        body = six.moves.urllib_parse.urlencode({
+        body = urlencode({
             'secret': conf.secret,
             'response': token,
             'remoteip': self.request.remote_ip
@@ -242,7 +242,7 @@ class GoogleAuth(AuthHandler, GoogleOAuth2Mixin):
             'key': self.kwargs['key'],
             'secret': self.kwargs['secret']
         }
-        redirect_uri = '{0.protocol:s}://{0.host:s}{0.path:s}'.format(self.request)
+        redirect_uri = self.xrequest_uri
         code = self.get_arg('code', '')
         if code:
             access = yield self.get_authenticated_user(
@@ -403,7 +403,7 @@ def csv_encode(values, *args, **kwargs):
     buf = StringIOClass()
     writer = csv.writer(buf, *args, **kwargs)
     writer.writerow([
-        v if isinstance(v, six.text_type) else
-        v.decode('utf-8') if isinstance(v, six.binary_type) else repr(v)
+        v if isinstance(v, str) else
+        v.decode('utf-8') if isinstance(v, bytes) else repr(v)
         for v in values])
     return buf.getvalue().strip()

gramex/handlers/basehandler.py
@@ -10,7 +10,7 @@ import tornado.gen
 import gramex.cache
 from binascii import b2a_base64, hexlify
 from orderedattrdict import AttrDict
-from six.moves.urllib_parse import urlparse, urlsplit, urljoin, urlencode
+from urllib.parse import urlsplit, urlunsplit, parse_qsl, urljoin, urlencode
 from tornado.web import RequestHandler, HTTPError, MissingArgumentError, decode_signed_value
 from tornado.websocket import WebSocketHandler
 from gramex import conf, __version__
@@ -185,7 +185,7 @@ class BaseMixin(object):
         this same consistent way.
         '''
         # Ensure that redirect is a dictionary before proceeding.
-        if isinstance(redirect, six.string_types):
+        if isinstance(redirect, str):
             redirect = {'url': redirect}
         if not isinstance(redirect, dict):
             app_log.error('url:%s.redirect must be a URL or a dict, not %s',
@@ -208,7 +208,7 @@ class BaseMixin(object):
                 def redirect_method(handler):
                     next_uri = method(handler)
                     if next_uri is not None:
-                        target = urlparse(next_uri)
+                        target = urlsplit(next_uri)
                         if not target.scheme and not target.netloc:
                             return next_uri
                         req = handler.request
@@ -365,7 +365,7 @@ class BaseMixin(object):
         for method in self.redirects:
             next_url = method(self)
             if next_url:
-                self.session['_next_url'] = urljoin(self.request.uri, next_url)
+                self.session['_next_url'] = urljoin(self.xrequest_uri, next_url)
                 return
         self.session.setdefault('_next_url', '/')
 
@@ -419,7 +419,7 @@ class BaseMixin(object):
                 self._write_headers(headers.items())
                 # result may be a generator / list from build_transform,
                 # or a str/bytes/unicode from Template.generate. Handle both
-                if isinstance(result, (six.string_types, six.binary_type)):
+                if isinstance(result, (str, bytes)):
                     self.write(result)
                 else:
                     for item in result:
@@ -591,7 +591,7 @@ class BaseHandler(RequestHandler, BaseMixin):
         # Convert this to proper unicode using UTF-8 and store in self.args
         self.args = {}
         for k in self.request.arguments:
-            key = (k if isinstance(k, six.binary_type) else k.encode('latin-1')).decode('utf-8')
+            key = (k if isinstance(k, bytes) else k.encode('latin-1')).decode('utf-8')
             # Invalid unicode (e.g. ?x=%f4) throws HTTPError. This disrupts even
             # error handlers. So if there's invalid unicode, log & continue.
             try:
@@ -634,6 +634,9 @@ class BaseHandler(RequestHandler, BaseMixin):
         return self.args[name][0 if first else -1]
 
     def prepare(self):
+        # If X-Request-URI domain is specified, use it. Else use RELATIVE URL, which allows nginx
+        # to proxy_redirect Location headers
+        self.xrequest_uri = self.request.headers.get('X-Request-URI', self.request.uri)
         for method in self._on_init_methods:
             method(self)
 
@@ -666,20 +669,17 @@ class BaseHandler(RequestHandler, BaseMixin):
         if not self.current_user:
             # Redirect non-AJAX requests GET/HEAD to login URL (if it's a string)
             ajax = self.request.headers.get('X-Requested-With', '').lower() == 'xmlhttprequest'
-            auth = getattr(self, '_auth', {})
-            if self.request.method in ('GET', 'HEAD') and not ajax:
+            if not ajax and self.request.method in ('GET', 'HEAD'):
+                auth = getattr(self, '_auth', {})
                 url = auth.get('login_url', self.get_login_url())
-                # If login_url is a string, redirect
-                if isinstance(url, six.string_types):
-                    if '?' not in url:
-                        if urlsplit(url).scheme:
-                            # if login url is absolute, make next absolute too
-                            next_url = self.request.full_url()
-                        else:
-                            next_url = self.request.uri
-                        next_key = auth.get('query', 'next')
-                        url += '?' + urlencode({next_key: next_url})
-                    self.redirect(url)
+                # If login_url: false, don't redirect to a login URL. Only redirect if it's a URL
+                if isinstance(url, str):
+                    # Redirect to the login_url adding ?next=<X-Request-URI>
+                    p = urlsplit(url)
+                    q = parse_qsl(p.query)
+                    q.append((auth.get('query', 'next'), self.xrequest_uri))
+                    target = urlunsplit((p.scheme, p.netloc, p.path, urlencode(q), p.fragment))
+                    self.redirect(target)
                     return
             # Else, send a 401 header
             raise HTTPError(UNAUTHORIZED)
@@ -768,8 +768,8 @@ class BaseHandler(RequestHandler, BaseMixin):
         '''
         result = AttrDict()
 
-        args_type = six.text_type
-        if len(args) > 0 and args[0] in (six.text_type, six.binary_type, list, None):
+        args_type = str
+        if len(args) > 0 and args[0] in (str, bytes, list, None):
             args_type, args = args[0], args[1:]
 
         for key in args:
@@ -831,7 +831,7 @@ class BaseHandler(RequestHandler, BaseMixin):
             for key, val in self.args.items():
                 if key not in args and key not in kwargs:
                     result[key] = val
-        elif args_type in (six.string_types, six.binary_type):
+        elif args_type in (str, bytes):
             for key, val in self.args.items():
                 if key not in args and key not in kwargs:
                     result[key] = args_type(val[0])
@@ -941,9 +941,7 @@ handle_cache = {}
 def _handle(path):
     '''Returns a cached append-binary handle to path'''
     if path not in handle_cache:
-        # In Python 2, csv writerow writes byte string. In PY3, it's to a unicode string.
-        # Open file handles accordingly
-        handle_cache[path] = open(path, 'ab') if six.PY2 else io.open(path, 'a', encoding='utf-8')
+        handle_cache[path] = io.open(path, 'a', encoding='utf-8')
     return handle_cache[path]
 
 

gramex/handlers/capturehandler.py
@@ -1,6 +1,5 @@
 import re
 import os
-import six
 import json
 import time
 import shlex
@@ -11,10 +10,10 @@ import tornado.gen
 from orderedattrdict import AttrDict
 from threading import Thread, Lock
 from subprocess import Popen, PIPE, STDOUT      # nosec
-from six.moves.urllib.parse import urlencode, urljoin
+from urllib.parse import urlencode, urljoin
 from tornado.web import HTTPError
 from tornado.httpclient import AsyncHTTPClient
-from gramex.config import app_log, variables, recursive_encode
+from gramex.config import app_log, variables
 from gramex.http import OK, BAD_REQUEST, GATEWAY_TIMEOUT, BAD_GATEWAY, CLIENT_TIMEOUT
 from .basehandler import BaseHandler
 
@@ -191,8 +190,6 @@ class Capture(object):
                 yield tornado.gen.sleep(self.check_interval)
         if not self.started:
             raise RuntimeError('%s not started. See logs' % self.engine.script)
-        if six.PY2:
-            recursive_encode(kwargs)
         r = yield self.browser.fetch(
             self.url, method='POST', body=urlencode(kwargs, doseq=True), raise_error=False,
             connect_timeout=self.timeout, request_timeout=self.timeout, headers=headers)
@@ -328,7 +325,10 @@ class CaptureHandler(BaseHandler):
             raise HTTPError(BAD_REQUEST, reason='%s: CaptureHandler needs ?url=' % self.name)
 
         # If the URL is a relative URL, treat it relative to the called path
-        args['url'] = urljoin(self.request.full_url(), args['url'])
+        # If xrequest_uri is specified and is a full URL, use that as the base.
+        # Else use the full request URL as the backup.
+        base = urljoin(self.request.full_url(), self.xrequest_uri)
+        args['url'] = urljoin(base, args['url'])
         # Copy all relevant HTTP headers as-is
         args['headers'] = {
             key: val for key, val in self.request.headers.items()

gramex/handlers/socialhandler.py
@@ -1,5 +1,4 @@
 import os
-import six
 import json
 import gramex
 import tornado.gen
@@ -53,7 +52,7 @@ class SocialMixin(object):
         if content and response.code == OK:
             content = yield gramex.service.threadpool.submit(self.run_transforms, content=content)
         # Convert to JSON if required
-        if not isinstance(content, (six.binary_type, six.text_type)):
+        if not isinstance(content, (str, bytes)):
             content = json.dumps(content, ensure_ascii=True, separators=(',', ':'))
         raise tornado.gen.Return(content)
 
@@ -195,8 +194,7 @@ class TwitterRESTHandler(SocialMixin, BaseHandler, TwitterMixin):
             self.redirect_next()
         else:
             self.save_redirect_page()
-            yield self.authorize_redirect(callback_uri=self.request.protocol + "://" +
-                                          self.request.host + self.request.uri)
+            yield self.authorize_redirect(callback_uri=self.xrequest_uri)
 
     def _oauth_consumer_token(self):
         return dict(key=self.kwargs['key'],
@@ -255,7 +253,7 @@ class FacebookGraphHandler(SocialMixin, BaseHandler, FacebookGraphMixin):
 
     @tornado.gen.coroutine
     def login(self):
-        redirect_uri = self.request.protocol + "://" + self.request.host + self.request.uri
+        redirect_uri = self.xrequest_uri
         if self.get_argument('code', False):
             info = self.session[self.user_info] = yield self.get_authenticated_user(
                 redirect_uri=redirect_uri,

tests/test_auth.py
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 from nose.tools import eq_, ok_
 from nose.plugins.skip import SkipTest
 from tornado.web import create_signed_value
-from six.moves.urllib_parse import urlencode
+from urllib.parse import urlencode, urljoin
 import gramex
 import gramex.config
 from gramex.cache import SQLiteStore
@@ -114,14 +114,14 @@ class AuthBase(TestGramex):
         r = self.login(*args, **kwargs)
         eq_(r.status_code, OK)
         self.assertNotRegexpMatches(r.text, 'error code')
-        eq_(r.url, server.base_url + check_next)
+        eq_(r.url, urljoin(server.base_url, check_next))
 
     def logout_ok(self, *args, **kwargs):
         check_next = kwargs.pop('check_next')
         # logout() does not accept user, password. So Just pass the kwargs
         r = self.logout(**kwargs)
         eq_(r.status_code, OK)
-        eq_(r.url, server.base_url + check_next)
+        eq_(r.url, urljoin(server.base_url, check_next))
 
     def unauthorized(self, *args, **kwargs):
         r = self.login(*args, **kwargs)
@@ -138,7 +138,7 @@ class AuthBase(TestGramex):
             if 'data' in kwargs:
                 data.update(kwargs.pop('data'))
             r = session.post(self.url, data=data, allow_redirects=False, **kwargs)
-            eq_(r.headers['Location'], url)
+            eq_(urljoin(server.base_url, r.headers['Location']), urljoin(server.base_url, url))
 
 
 class LoginMixin(object):

gramex/handlers/filehandler.py
@@ -6,12 +6,11 @@ import tornado.web
 import tornado.gen
 from pathlib import Path
 from fnmatch import fnmatch
-from six import string_types, text_type
 from tornado.escape import utf8
 from tornado.web import HTTPError
 from collections import defaultdict
 from orderedattrdict import AttrDict
-from six.moves.urllib.parse import urljoin
+from urllib.parse import urljoin, urlsplit, urlunsplit
 from .basehandler import BaseHandler
 from gramex.config import objectpath, app_log
 from gramex import conf as gramex_conf
@@ -160,7 +159,7 @@ class FileHandler(BaseHandler):
         for pattern in result:
             if not pattern:
                 app_log.warning('%s: Ignoring empty pattern "%r"', cls.name, pattern)
-            elif not isinstance(pattern, string_types):
+            elif not isinstance(pattern, (str, bytes)):
                 app_log.warning('%s: pattern "%r" is not a string. Ignoring.', cls.name, pattern)
             result.add(pattern)
         return result
@@ -183,7 +182,7 @@ class FileHandler(BaseHandler):
             for pattern, filestr in self.root.items():
                 match = pattern.match(path)
                 if match:
-                    q = defaultdict(text_type, **self.default)
+                    q = defaultdict(str, **self.default)
                     q.update({k: v[0] for k, v in self.args.items() if len(v) > 0})
                     q.update(match.groupdict())
                     p = Path(filestr.format(*match.groups(), **q)).absolute()
@@ -234,8 +233,9 @@ class FileHandler(BaseHandler):
                 raise HTTPError(NOT_FOUND, '%s missing index', self.file)
             # Ensure URL has a trailing '/' when displaying the index / default file
             if not self.request.path.endswith('/'):
-                suffix = '/?' + self.request.query if self.request.query else '/'
-                self.redirect(self.request.path + suffix, permanent=True)
+                p = urlsplit(self.xrequest_uri)
+                r = urlunsplit((p.scheme, p.netloc, p.path + '/', p.query, p.fragment))
+                self.redirect(r, permanent=True)
                 return
         else:
             self.file = self.path

gramex/handlers/formhandler.py
@@ -1,4 +1,3 @@
-import six
 import json
 import tornado.gen
 import gramex.cache
@@ -169,10 +168,10 @@ class FormHandler(BaseHandler):
         format_options['args'] = opt.args
         params = {k: v[0] for k, v in opt.args.items() if len(v) > 0}
         for key, val in format_options.items():
-            if isinstance(val, six.text_type):
+            if isinstance(val, str):
                 format_options[key] = val.format(**params)
             # In PY2, the values are binary. TODO: ensure that format values are in Unicode
-            elif isinstance(val, six.binary_type):
+            elif isinstance(val, bytes):
                 format_options[key] = val.decode('utf-8').format(**params)
         if opt.download:
             self.set_header('Content-Disposition', 'attachment;filename=%s' % opt.download)

gramex/handlers/processhandler.py
@@ -1,6 +1,5 @@
 import io
 import os
-import six
 import tornado.web
 import tornado.gen
 from threading import RLock
@@ -100,7 +99,7 @@ class ProcessHandler(BaseHandler):
             elif not target:
                 pass
             # strings are treated as files
-            elif isinstance(target, six.string_types):
+            elif isinstance(target, (str, bytes)):
                 # cache file handles for re-use between stdout, stderr
                 if target not in self.handles:
                     self.handles[target] = io.open(target, mode='wb')

commit c0ad553dde5e61be69697ff29f2b040f0b4e15b0

BUG: Fix auth redirection. Regression from f8edaeeb. Fixes #298

gramex/handlers/authhandler.py
@@ -239,11 +239,10 @@ class GoogleAuth(AuthHandler, GoogleOAuth2Mixin):
             'key': self.kwargs['key'],
             'secret': self.kwargs['secret']
         }
-        redirect_uri = self.xrequest_uri
         code = self.get_arg('code', '')
         if code:
             access = yield self.get_authenticated_user(
-                redirect_uri=redirect_uri,
+                redirect_uri=self.xredirect_uri,
                 code=code)
             user = yield self.oauth2_request(
                 'https://www.googleapis.com/oauth2/v1/userinfo',
@@ -263,7 +262,7 @@ class GoogleAuth(AuthHandler, GoogleOAuth2Mixin):
                 extra_params['approval_prompt'] = 'auto'
             # Return the list
             yield self.authorize_redirect(
-                redirect_uri=redirect_uri,
+                redirect_uri=self.xredirect_uri,
                 client_id=self.kwargs['key'],
                 scope=scope,
                 response_type='code',

gramex/handlers/basehandler.py
@@ -634,9 +634,15 @@ class BaseHandler(RequestHandler, BaseMixin):
         return self.args[name][0 if first else -1]
 
     def prepare(self):
-        # If X-Request-URI domain is specified, use it. Else use RELATIVE URL, which allows nginx
-        # to proxy_redirect Location headers
+        # If X-Request-URI is specified, use it. Else, when redirecting use RELATIVE URL. That
+        # allows nginx to proxy_redirect Location headers
         self.xrequest_uri = self.request.headers.get('X-Request-URI', self.request.uri)
+        # When passing to URL query parameters (e.g. /login/?next=), use the full URL
+        self.xrequest_full_url = urljoin(self.request.full_url(), self.xrequest_uri)
+        # For third-party redirection (e.g. Google Auth / Twitter needs a callback URI), then use
+        # the full URL WITHOUT query parameters
+        self.xredirect_uri = '{0.scheme:s}://{0.netloc:s}{0.path:s}'.format(
+            urlsplit(self.xrequest_full_url))
         for method in self._on_init_methods:
             method(self)
 
@@ -677,7 +683,7 @@ class BaseHandler(RequestHandler, BaseMixin):
                     # Redirect to the login_url adding ?next=<X-Request-URI>
                     p = urlsplit(url)
                     q = parse_qsl(p.query)
-                    q.append((auth.get('query', 'next'), self.xrequest_uri))
+                    q.append((auth.get('query', 'next'), self.xrequest_full_url))
                     target = urlunsplit((p.scheme, p.netloc, p.path, urlencode(q), p.fragment))
                     self.redirect(target)
                     return

gramex/handlers/capturehandler.py
@@ -324,11 +324,8 @@ class CaptureHandler(BaseHandler):
         if args['url'] is None:
             raise HTTPError(BAD_REQUEST, reason='%s: CaptureHandler needs ?url=' % self.name)
 
-        # If the URL is a relative URL, treat it relative to the called path
-        # If xrequest_uri is specified and is a full URL, use that as the base.
-        # Else use the full request URL as the backup.
-        base = urljoin(self.request.full_url(), self.xrequest_uri)
-        args['url'] = urljoin(base, args['url'])
+        # ?url= can be a relative URL. Use the full X-Request URL as the base
+        args['url'] = urljoin(self.xrequest_full_url, args['url'])
         # Copy all relevant HTTP headers as-is
         args['headers'] = {
             key: val for key, val in self.request.headers.items()

gramex/handlers/socialhandler.py
@@ -194,7 +194,7 @@ class TwitterRESTHandler(SocialMixin, BaseHandler, TwitterMixin):
             self.redirect_next()
         else:
             self.save_redirect_page()
-            yield self.authorize_redirect(callback_uri=self.xrequest_uri)
+            yield self.authorize_redirect(callback_uri=self.xredirect_uri)
 
     def _oauth_consumer_token(self):
         return dict(key=self.kwargs['key'],
@@ -253,10 +253,9 @@ class FacebookGraphHandler(SocialMixin, BaseHandler, FacebookGraphMixin):
 
     @tornado.gen.coroutine
     def login(self):
-        redirect_uri = self.xrequest_uri
         if self.get_argument('code', False):
             info = self.session[self.user_info] = yield self.get_authenticated_user(
-                redirect_uri=redirect_uri,
+                redirect_uri=self.xredirect_uri,
                 client_id=self.kwargs['key'],
                 client_secret=self.kwargs['secret'],
                 code=self.get_argument('code'))
@@ -267,6 +266,6 @@ class FacebookGraphHandler(SocialMixin, BaseHandler, FacebookGraphMixin):
             self.save_redirect_page()
             scope = self.kwargs.get('scope', 'user_posts,read_insights')
             yield self.authorize_redirect(
-                redirect_uri=redirect_uri,
+                redirect_uri=self.xredirect_uri,
                 client_id=self.kwargs['key'],
                 extra_params={'scope': scope})

tests/test_auth.py
@@ -671,7 +671,8 @@ class TestAuthorize(DBAuthBase, LoginFailureMixin):
     def initialize(self, url, user='alpha', login_url='/login/', next_key='next'):
         self.session = requests.Session()
         r = self.session.get(server.base_url + url)
-        eq_(r.url, server.base_url + login_url + '?' + urlencode({next_key: url}))
+        eq_(r.url, server.base_url + login_url + '?' + urlencode({
+            next_key: server.base_url + url}))
         r = self.session.post(server.base_url + url)
         eq_(r.url, server.base_url + url)
         eq_(r.status_code, UNAUTHORIZED)