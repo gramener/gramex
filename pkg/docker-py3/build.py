c5b3a1337d59d50b6156a86bec05de19854766dd
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2020-11-03 12:57:49 +0530

    BLD: Update Dockerfile to install gramex from conda (#322) Fixes #321
    * BLD: Update Dockerfile to install gramex from conda
    
    Fixes #321
    
    * tag version explicitly as latest, print stream w/o newlines
    
    * BLD: Install pandoc
    
    Required for guide/r
    
    Co-authored-by: S Anand <root.node@gmail.com>

--- pkg/docker-py3/build.py
+++ pkg/docker-py3/build.py
@@ -5,12 +5,16 @@ import io
 template = gramex.cache.open('Dockerfile.tmpl', 'template', rel=True)
 release = gramex.cache.open('../../gramex/release.json', rel=True)['info']
 client = docker.from_env()
+tag = f'gramener/gramex:{release["version"]}'
 streamer = client.api.build(
     fileobj=io.BytesIO(template.generate(**release)),
-    tag=f'gramener/gramex:{release["version"]}',
+    tag=tag,
     decode=True,
     rm=True,
 )
 for chunk in streamer:
     if 'stream' in chunk:
-        print(chunk['stream'])  # noqa
+        print(chunk['stream'], end='')      # noqa
+
+# Tag current version as latest
+client.api.tag(tag, 'gramener/gramex', 'latest')
