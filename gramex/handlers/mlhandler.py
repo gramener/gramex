a0c98fc2031618571db68ba2152ad466a8a835fc
S Anand <root.node@gmail.com>
2020-11-19 11:14:21 +0530

    Comments on TODOs

--- gramex/handlers/mlhandler.py
+++ gramex/handlers/mlhandler.py
@@ -20,6 +20,9 @@ from tornado.web import HTTPError
 
 op = os.path
 SCORES = defaultdict(list)
+# TODO: Can we just search across all possible namespaces for the class, and avoid this totally?
+# TODO: Include statsmodels as well. Do not include as an explicit dependency. Try to import
+# TODO: Fallback to libraries for search: sklearn - statsmodels - keras - ...
 SKLEARN_CLASSES = dict(
     LogisticRegression=sklearn.linear_model,
     RidgeClassifier=sklearn.linear_model,
@@ -32,7 +35,9 @@ SKLEARN_CLASSES = dict(
     MultinomialNB=sklearn.naive_bayes,
     RandomForestClassifier=sklearn.ensemble,
     DecisionTreeClassifier=sklearn.tree,
-    MLPClassifier=sklearn.neural_network
+    MLPClassifier=sklearn.neural_network,
+    LinearRegression=sklearn.linear_model,
+    DecisionTreeRegressor=sklearn.tree,
 )
 
 
@@ -53,6 +58,7 @@ class MLHandler(FormHandler):
             cls.model_path = op.join(variables['YAMLPATH'], op.basename(cls.name) + '.pkl')
         if op.isfile(path):
             cls.model = cache.open(path, joblib.load)
+            # TODO: If it's not the same as gramex.yaml model, then re-create with a warning
         else:
             class_module = SKLEARN_CLASSES.get(model_class, False)
             if not class_module:
@@ -104,6 +110,10 @@ class MLHandler(FormHandler):
 
     @coroutine
     def post(self, *path_args, **path_kwargs):
+        # TODO: [IMP]: Instead, concat all self.request.files read via gramex.cache.open with the
+        #   relevant filename.ext.
+        # TODO: This should append to a data store
+        # TODO: Support defining input columns, not just target columns
         data = pd.read_json(self.request.body.decode('utf8'))
         if self.get_arg('_retrain', False):
             score = yield gramex.service.threadpool.submit(self._fit, data)
@@ -115,6 +125,7 @@ class MLHandler(FormHandler):
 
     @coroutine
     def put(self, *path_args, **path_kwargs):
+        # TODO: If we change the model class, GET ?_model does not return the new one
         mclass = self.args.pop('class')[0]
         if not op.isfile(self.model_path):
             self.model = getattr(SKLEARN_CLASSES.get(mclass), mclass)(
