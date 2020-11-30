67de81b7f10fb7c7cd174e3bded2242768cf05e4
bhanu.k <bhanu.kamapantula@gramener.com>
2020-11-30 12:05:00 +0530

    FXD: remove device resolution controls
    it's not adding enough value yet, will reconsider later

--- gramex/apps/forms/js/embed.js
+++ gramex/apps/forms/js/embed.js
@@ -3,10 +3,8 @@
 $('[id^="gramexform-"]').each(function () {
   var $this = $(this)
   var url = document.currentScript.src.replace(/js\/embed.js$/, $this.attr('id').replace(/^gramexform-/, 'publish?id='))
-  console.log(url)
   $.getJSON(url)
     .done(function (data) {
-      console.log(data)
       $('.popover-element').template({ fields: JSON.parse(data[0].config).fields})
       // $this.formbuilder(config)
     })
--- gramex/apps/forms/js/view-form.js
+++ gramex/apps/forms/js/view-form.js
@@ -1,23 +1,5 @@
 /* globals form_id */
 
-const resolutions = {
-  laptop: {
-    resX: 1080,
-    resY: 1920,
-    devicePixelRatio: 3
-  },
-  tablet: {
-    resX: 640,
-    resY: 980,
-    devicePixelRatio: 2
-  },
-  mobile: {
-    resX: 640,
-    resY: 980,
-    devicePixelRatio: 2
-  }
-}
-
 $(function() {
   $('.btn.viewsource').addClass('d-none')
   $.ajax(`../embed/${form_id}.html`, {
@@ -49,33 +31,4 @@ $('body').on('click', 'button#toggle-source', function() {
     $('.btn.viewsource').addClass('d-none')
     $('.sourcecode-container').addClass('d-none')
   }
-}).on('click', 'button[data-device]', function() {
-  const device = $(this).data('device')
-  const width = resolutions[device].resX/resolutions[device].devicePixelRatio
-  const height = resolutions[device].resY/resolutions[device].devicePixelRatio
-
-  $('#preview').removeClass('d-none')
-
-  document.getElementById('preview').style.width = width + "px"
-  document.getElementById('preview-body').style.height = height + "px"
-  document.getElementById('frame').srcdoc = document.getElementById('view-form').innerHTML
-
-  document.getElementById('frame').width = width
-  document.getElementById('frame').height = height
-
-  // setTimeout since content needs to load before style is applied
-  setTimeout(function() {
-    let $head = $("#frame").contents().find("head")
-    $head.append($("<link/>"), {
-      rel: "stylesheet",
-      href: "https://fonts.googleapis.com/css2?family=Comfortaa&display=swap"
-    })
-    $head.append($("<link/>", {
-      rel: "stylesheet",
-      href: "../style.css",
-      type: "text/css"
-    }))
-    // TODO: font doesn't get applied to iframe content yet
-    document.getElementById("frame").contentDocument.body.style.fontFamily = "Comfortaa"
-  }, 100) // setTimeout
-}) // click event
+})
