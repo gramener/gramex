ff464136a2ec8780d71b3c45aa3e640f18cea8fc
S Anand <root.node@gmail.com>
2022-11-08 13:59:47 +0530

    ENH: API ratelimit available for all handlers. Fixes #632 (#635)
    * ENH: API ratelimit available for all handlers
    * DOC: fix docstring for BaseMixin.get_list

--- gramex/transforms/__init__.py
+++ gramex/transforms/__init__.py
@@ -3,7 +3,7 @@
 from .auth import ensure_single_session
 from .template import template, sass, scss, ts, vue
 from .transforms import build_transform, build_pipeline, build_log_info, condition, flattener, once
-from .transforms import handler, Header
+from .transforms import handler, handler_expr, Header
 
 # Import common libraries with their popular abbreviations.
 # This lets build_transform() to use, for e.g., `pd.concat()` instead of `pandas.concat()`.
@@ -24,6 +24,7 @@ __all__ = [
     'flattener',
     'once',
     'handler',
+    'handler_expr',
     'Header',
     'pd',
     'np',
