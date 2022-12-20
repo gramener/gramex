c097854b87d7fc99be02833bf57ecef10a861e99
S Anand <root.node@gmail.com>
2022-12-20 23:16:09 +0800

    DOC: Add v1.86.1 release changes

--- gramex/__init__.py
+++ gramex/__init__.py
@@ -52,7 +52,7 @@ gramex run                    Run an installed app
 gramex uninstall              Uninstall an app
 '''
 
-__version__ = '1.86.0'
+__version__ = '1.86.1'
 
 paths = AttrDict()  # Paths where configurations are stored
 conf = AttrDict()  # Final merged configurations
