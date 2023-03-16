bb7798710b60bb7f405e87f76d2e8e02cd6f5b44
Shraddheya Shrivastava <shraddheya.shrivastava@gmail.com>
2023-03-16 14:42:00 +0530

    ADD: test cases for ilike

--- testlib/test_data.py
+++ testlib/test_data.py
@@ -150,6 +150,11 @@ class TestFilter(unittest.TestCase):
         )
 
         m = eq(
+            {'city': ['Hyderabad', 'Coimbatore', ''], 'c1': [''], 'c2>': [''], 'city*': ['']},
+            sales[sales['city'].isin(['Hyderabad', 'Coimbatore'])],
+        )
+
+        m = eq(
             {'city>~': ['Bangalore'], 'city<~': ['Singapore']},
             sales[(sales['city'] >= 'Bangalore') & (sales['city'] <= 'Singapore')],
         )
@@ -157,6 +162,9 @@ class TestFilter(unittest.TestCase):
 
         m = eq({'city~': ['ore']}, sales[sales['city'].str.contains('ore')])
         eq_(m['filters'], [('city', '~', ('ore',))])
+
+        m = eq({'city*': ['ore']}, sales[sales['city'].str.contains('ore')])
+        eq_(m['filters'], [('city', '*', ('ore',))])
 
         m = eq(
             {'product': ['Biscuit'], 'city': ['Bangalore'], 'देश': ['भारत']},
@@ -173,6 +181,9 @@ class TestFilter(unittest.TestCase):
 
         m = eq({'city!~': ['ore']}, sales[~sales['city'].str.contains('ore')])
         eq_(m['filters'], [('city', '!~', ('ore',))])
+
+        m = eq({'city!*': ['ore']}, sales[~sales['city'].str.contains('ore')])
+        eq_(m['filters'], [('city', '!*', ('ore',))])
 
         m = eq(
             {'sales>': ['100'], 'sales<': ['1000']},
@@ -488,9 +499,14 @@ class TestFilter(unittest.TestCase):
         size(args={'sales>~': ['200']}, b=8)
         size(args={'sales<~': ['41.9']}, b=8)
         size(args={'city~': ['South']}, b=4)
+        size(args={'city*': ['south']}, b=4)
+        size(args={'city*': ['south']}, b=4)
         size(args={'city!~': ['Newport']}, b=20)
+        size(args={'city!*': ['Newport']}, b=20)
         size(args={'sales>': ['20'], 'sales<': ['500']}, b=13)
         size(args={'city~': ['South'], 'product': ['Biscuit']}, b=1)
+        size(args={'city*': ['South'], 'product': ['Biscuit']}, b=1)
+        size(args={'city*': ['south'], 'product': ['biscuit']}, b=1)
         # TODO: NOT NULL
         # TEST size(args={'sales!': []}, b=2)
         # TEST size(args={'sales': []}, b=22)
