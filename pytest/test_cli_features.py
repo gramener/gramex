f5989332b6a83158ae57ccafa5a6a2ab5f7fb13e
Shraddheya Shrivastava <shraddheya.shrivastava@gmail.com>
2023-03-21 12:52:03 +0530

    ADD: test for features subcommand

---
+++ pytest/test_cli_features.py
@@ -0,0 +1,15 @@
+import os
+import json
+import glob
+from gramex.install import features
+
+
+def test_cli_features():
+    for root in glob.glob('pytest/features-*'):    
+        path = os.path.join(os.getcwd(), root)
+        actual = features([path], {})
+        expected_path = os.path.join(path, 'expected.json')
+
+        with open(expected_path, 'r') as expected:
+            expected = json.load(expected)
+            assert expected == actual
