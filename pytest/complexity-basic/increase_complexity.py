1965a6f3e9fcbeaa1257817a51cae78b85cbb0e8
Shraddheya Shrivastava <shraddheya.shrivastava@gmail.com>
2023-03-21 12:07:15 +0530

    FIX: test case for complexity
    according to the comments in the PR

---
+++ pytest/complexity-basic/increase_complexity.py
@@ -0,0 +1,40 @@
+"""_summary_
+this file will be considered in complexity calculation
+"""
+
+# Complexity O(m*n)
+def lcs(X, Y):
+    m = len(X)
+    n = len(Y)
+    L = [[0] * (n+1) for i in range(m+1)]
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            if X[i-1] == Y[j-1]:
+                L[i][j] = L[i-1][j-1] + 1
+            else:
+                L[i][j] = max(L[i-1][j], L[i][j-1])
+    return L[m][n]
+
+# Complexity: O(n log n) in worst case
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    return merge(left, right)
+
+
+def merge(left, right):
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result += left[i:]
+    result += right[j:]
+    return result
--- pytest/test_cli_features.py
+++ pytest/test_cli_features.py
@@ -1,32 +1,15 @@
 import os
 import json
-import pytest
-import importlib.util
+import glob
+from gramex.install import complexity
 
-def get_test_directories():
-    test_dirs = []
-    for root, dirs, files in os.walk(os.getcwd()):
-        if 'actual.py' in files and 'expected.json' in files:
-            test_dirs.append(root)
-    return test_dirs
 
-@pytest.fixture(params=get_test_directories())
-def actual_and_expected(request):
-    root = request.param
-    os.chdir(root)
-    actual_path = os.path.join(root, 'actual.py')
-    expected_path = os.path.join(root, 'expected.json')
+def test_actual_vs_expected():
+    for root in glob.glob('pytest/complexity-*'):    
+        path = os.path.join(os.getcwd(), root)
+        actual = complexity([path], {})
+        expected_path = os.path.join(path, 'expected.json')
 
-    spec = importlib.util.spec_from_file_location("actual", actual_path)
-    actual_module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(actual_module)
-    actual = actual_module.actual([], {})
-
-    with open(expected_path, 'r') as expected:
-        expected_result = json.load(expected)
-
-    return actual, expected_result
-
-def test_actual_vs_expected(actual_and_expected):
-    actual, expected = actual_and_expected
-    assert actual == expected
+        with open(expected_path, 'r') as expected:
+            expected = json.load(expected)
+            assert expected == actual
