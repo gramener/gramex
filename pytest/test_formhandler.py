00a13b9ccb61ab2c966a7f04e642ecd2ca682bd4
Shraddheya Shrivastava <shraddheya.shrivastava@gmail.com>
2023-04-06 11:46:43 +0530

    FIX: join test
    fix the failing test because of malformed inputs in the yaml

--- pytest/test_formhandler.py
+++ pytest/test_formhandler.py
@@ -20,12 +20,12 @@ products_data: pd.DataFrame = gramex.cache.open(sales_join_file, sheet_name="pro
 @contextmanager
 def sqlite():
     yield dbutils.sqlite_create_db(
-        "test_delete.db",
+        "test_formhandler_join.db",
         sales=sales_join_data,
         customers=customers_data,
         products=products_data,
     )
-    dbutils.sqlite_drop_db("test_delete.db")
+    dbutils.sqlite_drop_db("test_formhandler_join.db")
 
 
 db_setups = [
@@ -37,19 +37,21 @@ db_setups = [
 
 
 @pytest.mark.parametrize(
-    "result,db_setup", product(glob(os.path.join(folder, "formhandler-*", "*.yaml")), db_setups)
+    "result,db_setup",
+    product(glob(os.path.join(folder, "formhandler-*-contr*", "*.yaml")), db_setups),
 )
 def test_formhandler_join(result, db_setup):
     resJson = gramex.cache.open(result)
-    kwargs, formatting = resJson["kwargs"], resJson["formatting"]
     args = []
-    if 'args' in resJson:
-        args = resJson['args']
+    if "args" in resJson:
+        args = resJson["args"]
     with db_setup() as url:
-        kwargs["url"] = url
+        resJson["kwargs"]["url"] = url
+        actual = gramex.data.filter(args=args, meta={}, **resJson["kwargs"])
         expected = pd.read_sql(resJson["expected"], url)
-        actual = gramex.data.filter(args=args, meta={}, **kwargs)
-        for k, v in formatting.items():
-            fun = getattr(pd, v)
-            expected[k] = expected[k].apply(fun)
-        afe(expected, actual)
+        if not expected.empty and "formatting" in resJson:
+            for k, v in resJson["formatting"].items():
+                fun = getattr(pd, v)
+                expected[k] = expected[k].apply(fun)
+
+    afe(expected, actual)
