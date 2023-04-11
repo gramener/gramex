66ebebbca63c091a577f1008d4af96f52a5c8585
S Anand <root.node@gmail.com>
2023-04-11 20:20:18 +0800

    DOC: Add v1.89.2 release changes

--- pytest/complexity-basic/subfolder/test.js
+++ pytest/complexity-basic/subfolder/test.js
@@ -10,18 +10,19 @@ describe("Calculate complexity of each folder", () => {
   const dirs = readdirSync(__dirname);
   for (let dir of dirs) {
     const path = join(__dirname, dir);
-    if (statSync(path).isDirectory() && dir.startsWith('js-')) {
+    if (statSync(path).isDirectory() && dir.startsWith("js-")) {
       test(dir, async () => {
         const actual = [];
         for await (const result of esComplexity(path)) {
           actual.push(result);
-          if (result.type === 'estimate')
-            expect(result.error).toBeDefined();
-            delete result.error;
+          if (result.type === "estimate") expect(result.error).toBeDefined();
+          delete result.error;
         }
-        const expected = JSON.parse(readFileSync(join(__dirname, dir, 'expected.json')));
+        const expected = JSON.parse(
+          readFileSync(join(__dirname, dir, "expected.json"))
+        );
         expect(actual).toStrictEqual(expected);
       });
     }
   }
-})
+});
