b5b895bd8b074fe0c551415635d735023afbb694
S Anand <root.node@gmail.com>
2023-04-24 17:42:37 +0800

    STY: Fix build errors (#714)

--- gramex/apps/admin2/rollup.config.js
+++
@@ -1,9 +0,0 @@
-import htmlparts from "rollup-plugin-htmlparts";
-
-export default [
-  {
-    input: "schedule.src.js",
-    output: { file: "schedule.js", name: "schedule", format: "umd" },
-    plugins: [htmlparts("schedule.template.html")],
-  },
-];
