ef82bcfd0a36f5fef85f824bb0abfa4801151e97
S Anand <root.node@gmail.com>
2023-04-24 12:47:25 +0530

    STY: Fix more build errors

--- pytest/test_cli.py
+++ pytest/test_cli.py
@@ -14,14 +14,14 @@ def test_features_empty():
     assert features(['/nonexistent'], {}) == ''
 
 
-@pytest.mark.parametrize("root", glob(os.path.join(folder, 'features-*')))
+@pytest.mark.parametrize("root", glob(os.path.join(folder, 'features_*')))
 def test_features(root):
     actual = features([root], {'format': 'csv'})
     expected = gramex.cache.open(os.path.join(root, 'expected.csv'), 'text')
     assert expected == actual
 
 
-@pytest.mark.parametrize("root", glob(os.path.join(folder, 'complexity-*')))
+@pytest.mark.parametrize("root", glob(os.path.join(folder, 'complexity_*')))
 def test_complexity(root):
     actual = complexity([root], {})
     expected = read_json(os.path.join(root, 'expected.json'))
